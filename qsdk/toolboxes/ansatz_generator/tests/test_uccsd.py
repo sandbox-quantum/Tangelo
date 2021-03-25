cf76df051eac34326d1e0466cc372dfef41415f4
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-03-25 01:06:15 -0700

    New init.py files, new setup and travis files, flattened folder structure for toolboxes

---
+++ qsdk/toolboxes/ansatz_generator/tests/test_uccsd.py
@@ -0,0 +1,100 @@
+import unittest
+import numpy as np
+from pyscf import gto
+
+from qsdk.toolboxes.molecular_computation.molecular_data import MolecularData
+from qsdk.toolboxes.vqe.qubit_mappings import jordan_wigner
+from qsdk.toolboxes.vqe.ansatz_generator.uccsd import UCCSD
+
+from agnostic_simulator import Simulator
+
+# Build molecule objects used by the tests
+H2 = [("H", (0., 0., 0.)), ("H", (0., 0., 0.7414))]
+H4 = [['H', [0.7071067811865476, 0.0, 0.0]], ['H', [0.0, 0.7071067811865476, 0.0]],
+      ['H', [-1.0071067811865476, 0.0, 0.0]], ['H', [0.0, -1.0071067811865476, 0.0]]]
+
+mol_h2 = gto.Mole()
+mol_h2.atom = H2
+mol_h2.basis = "sto-3g"
+mol_h2.spin = 0
+mol_h2.build()
+
+mol_h4 = gto.Mole()
+mol_h4.atom = H4
+mol_h4.basis = "sto-3g"
+mol_h4.spin = 0
+mol_h4.build()
+
+
+class UCCSDTest(unittest.TestCase):
+
+    def test_uccsd_H2(self):
+        """ Verify closed-shell UCCSD functionalities for H2 """
+
+        molecule = MolecularData(mol_h2)
+
+        # Build circuit
+        uccsd_ansatz = UCCSD(molecule)
+        uccsd_ansatz.build_circuit()
+
+        # Build qubit hamiltonian for energy evaluation
+        molecular_hamiltonian = molecule.get_molecular_hamiltonian()
+        qubit_hamiltonian = jordan_wigner(molecular_hamiltonian)
+
+        # Assert energy returned is as expected for given parameters
+        sim = Simulator(target="qulacs")
+        uccsd_ansatz.update_var_params([5.86665842e-06, 0.0565317429])
+        energy = sim.get_expectation_value(qubit_hamiltonian, uccsd_ansatz.circuit)
+        self.assertAlmostEqual(energy, -1.137270174551959, delta=1e-6)
+
+    def test_uccsd_H4(self):
+        """ Verify closed-shell UCCSD functionalities for H4 """
+
+        molecule = MolecularData(mol_h4)
+        var_params = [-3.96898484e-04, 4.59786847e-05, 3.95285013e-05, 1.85885610e-04, 1.05759154e-02,
+                      3.47363359e-01, 3.42657596e-02, 1.45006203e-02, 7.43941871e-02, 7.57255601e-03,
+                      -1.83407761e-01, -1.03261491e-01, 1.34258277e-02, -3.78096407e-02]
+
+        # Build circuit
+        uccsd_ansatz = UCCSD(molecule)
+        uccsd_ansatz.build_circuit()
+
+        # Build qubit hamiltonian for energy evaluation
+        molecular_hamiltonian = molecule.get_molecular_hamiltonian()
+        qubit_hamiltonian = jordan_wigner(molecular_hamiltonian)
+
+        # Assert energy returned is as expected for given parameters
+        sim = Simulator(target="qulacs")
+        uccsd_ansatz.update_var_params(var_params)
+        energy = sim.get_expectation_value(qubit_hamiltonian, uccsd_ansatz.circuit)
+        self.assertAlmostEqual(energy, -1.9778041, delta=1e-6)
+
+    def test_uccsd_H2_MP2(self):
+        """ Verify closed-shell UCCSD functionalities for H2: MP2 initial parameters """
+
+        molecule = MolecularData(mol_h2)
+
+        uccsd_ansatz = UCCSD(molecule)
+        uccsd_ansatz.var_params_initialization = "MP2"
+        uccsd_ansatz.initialize_var_params()
+
+        expected = [2e-05, 0.0363253711023451]
+        self.assertAlmostEqual(np.linalg.norm(uccsd_ansatz.var_params), np.linalg.norm(expected), delta=1e-10)
+
+    def test_uccsd_H4_MP2(self):
+        """ Verify closed-shell UCCSD functionalities for H4: MP2 initial parameters """
+
+        molecule = MolecularData(mol_h4)
+
+        uccsd_ansatz = UCCSD(molecule)
+        uccsd_ansatz.var_params_initialization = "MP2"
+        uccsd_ansatz.initialize_var_params()
+
+        expected = [2e-05, 2e-05, 2e-05, 2e-05, 0.03894901872789466, 0.07985689676283764, 0.02019977190077326,
+                    0.03777151472046017, 0.05845449631119356, 0.017956568628560945, -0.07212522602179856,
+                    -0.03958975799697206, -0.042927857009029735, -0.025307140867721886]
+        self.assertAlmostEqual(np.linalg.norm(uccsd_ansatz.var_params), np.linalg.norm(expected), delta=1e-10)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ qsdk/toolboxes/ansatz_generator/uccsd.py
@@ -0,0 +1,162 @@
+""" This module defines the ansatz abstract class, providing the foundation to implement variational ansatz circuits """
+
+import itertools
+import numpy as np
+from pyscf import mp
+
+from agnostic_simulator import Circuit, Gate
+
+from .ansatz import Ansatz
+from .ansatz_utils import pauliword_to_circuit
+from ._unitary_cc import uccsd_singlet_generator
+from ..qubit_mappings import jordan_wigner
+from qsdk.toolboxes.molecular_computation.integral_calculation import prepare_mf_RHF
+
+
+class UCCSD(Ansatz):
+    """ This class implements the UCCSD ansatz. Currently, only closed-shell UCCSD is supported.
+     This implies that the mean-field is computed with the RHF reference integrals. """
+
+    def __init__(self, molecule, mean_field=None):
+
+        self.molecule = molecule
+        self.mf = mean_field
+
+        # Later: refactor to handle various flavors of UCCSD
+        if molecule.n_qubits % 2 != 0:
+            raise ValueError('The total number of spin-orbitals should be even.')
+        self.n_spatial_orbitals = self.molecule.n_qubits // 2
+        self.n_occupied = int(np.ceil(self.molecule.n_electrons / 2))
+        self.n_virtual = self.n_spatial_orbitals - self.n_occupied
+        self.n_singles = self.n_occupied * self.n_virtual
+        self.n_doubles = self.n_singles * (self.n_singles + 1) // 2
+        self.n_var_params = self.n_singles + self.n_doubles
+
+        # Supported reference state initialization
+        # TODO: support for others and compatibility with the qubit mapping used
+        self.supported_reference_state = {"HF"}
+        # Supported var param initialization
+        self.supported_initial_var_params = {"ones", "random", "MP2"}
+
+        # Default initial parameters for initialization
+        self.var_params_initialization = "MP2"
+        self.reference_state_initialization = "HF"
+
+        self.var_params = None
+        self.circuit = None
+
+    def initialize_var_params(self):
+        """ Compute sets of potential initial values for variational parameters, such as zeros, random numbers, MP2,
+        or any insightful values. Impacts the convergence of the variational algorithm. """
+
+        if self.var_params_initialization not in self.supported_initial_var_params:
+            raise ValueError(f"Only supported initialization methods for variational parameters are:"
+                             f"{self.supported_initial_var_params} ")
+
+        if self.var_params_initialization == "ones":
+            initial_var_params = np.ones((self.n_var_params,), dtype=np.float)
+        elif self.var_params_initialization == "random":
+            initial_var_params = np.random.random((self.n_var_params,), dtype=np.float)
+        elif self.var_params_initialization == "MP2":
+            initial_var_params = self._compute_mp2_params()
+        self.var_params = initial_var_params
+        return initial_var_params
+
+    # TODO: Possible initial states must be compatible with qubit transform used, add check for it later on
+    def prepare_reference_state(self):
+        """ Returns circuit preparing the reference state of the ansatz (e.g prepare reference wavefunction with HF,
+        multi-reference state, etc). These preparations must be consistent with the transform used to obtain the
+        qubit operator.
+        """
+
+        if self.reference_state_initialization not in self.supported_reference_state:
+            raise ValueError(f"Only supported reference state methods are:{self.supported_reference_state}")
+
+        # NB: this one is consistent with JW but not other transforms.
+        if self.reference_state_initialization == "HF":
+            return Circuit([Gate("X", target=i) for i in range(self.molecule.n_electrons)])
+
+    def build_circuit(self, var_params=None):
+        """ Build and return the quantum circuit implementing the state preparation ansatz
+         (with currently specified initial_state and var_params) """
+
+        # Set initial variational parameters used to build the circuit
+        if var_params is not None: # Temporary, will be replaced once set_var_params can directly update the parameters
+            assert(len(var_params) == self.n_var_params)
+            self.var_params = var_params
+        elif not self.var_params:
+            self.initialize_var_params()
+
+        # Build qubit operator required to build UCCSD
+        ferm_op = uccsd_singlet_generator(self.var_params, self.molecule.n_qubits, self.molecule.n_electrons)
+        qubit_op = jordan_wigner(ferm_op)
+        # Cast all coefs to floats (rotations angles are real)
+        for key in qubit_op.terms:
+            qubit_op.terms[key] = float(qubit_op.terms[key].imag)
+        qubit_op.compress()
+
+        # Prepend reference state circuit
+        reference_state_circuit = self.prepare_reference_state()
+
+        # Obtain quantum circuit through trivial trotterization of the qubit operator
+        # Keep track of the order in which pauli words have been visited for fast subsequent parameter updates
+        pauli_words = sorted(qubit_op.terms.items(), key=lambda x: len(x[0]))
+        pauli_words_gates = []
+        self.pauli_to_angles_mapping = dict()
+        for i, (pauli_word, coef) in enumerate(pauli_words):
+            pauli_words_gates += pauliword_to_circuit(pauli_word, coef)
+            self.pauli_to_angles_mapping[pauli_word] = i
+
+        self.circuit = reference_state_circuit + Circuit(pauli_words_gates)
+
+    def update_var_params(self, var_params):
+        """ Shortcut: set value of variational parameters in the already-built ansatz circuit member.
+            The circuit does not need to be rebuilt every time if only the variational parameters change. """
+
+        self.var_params = var_params
+
+        # Build qubit operator required to build UCCSD
+        ferm_op = uccsd_singlet_generator(var_params, self.molecule.n_qubits, self.molecule.n_electrons)
+        qubit_op = jordan_wigner(ferm_op)
+        for key in qubit_op.terms:
+            qubit_op.terms[key] = float(qubit_op.terms[key].imag)
+        qubit_op.compress()
+
+        # If qubit operator terms haven't changed, perform fast parameter update
+        if set(self.pauli_to_angles_mapping.keys()) != set(qubit_op.terms.keys()):
+            print(f"Pauli words in qubit operator have changed, rebuilding UCCSD ansatz circuit.")
+            self.build_circuit(var_params)
+        else:
+            # Directly update angles in variational gates without rebuilding the circuit
+            for pauli_word, coef in qubit_op.terms.items():
+                gate_index = self.pauli_to_angles_mapping[pauli_word]
+                self.circuit._variational_gates[gate_index].parameter = 2.*coef if coef >= 0. else 4*np.pi+2*coef
+
+    def _compute_mp2_params(self):
+        """ Computes the MP2 initial variational parameters.
+        Compute the initial variational parameters with PySCF MP2 calculation, and then reorders the elements
+        into the QEMIST convention. MP2 only has doubles (T2) amplitudes, thus the single (T1) amplitudes are set to a
+        small non-zero value and added. The ordering for QEMIST is single, double (diagonal), double (non-diagonal).
+
+        Returns:
+            list: The initial variational parameters (float64).
+        """
+
+        # If no mean-field was passed, compute it using the module to do so, for the right integrals
+        if not self.mf:
+            self.mf = prepare_mf_RHF(self.molecule.mol)
+
+        mp2_fragment = mp.MP2(self.mf)
+        mp2_fragment.verbose = 0
+        mp2_correlation_energy, mp2_t2 = mp2_fragment.kernel()
+
+        # Get singles amplitude. Just get "up" amplitude, since "down" should be the same
+        singles = [2.e-5] * (self.n_virtual * self.n_occupied)
+        # Get singles and doubles amplitudes associated with one spatial occupied-virtual pair
+        doubles_1 = [-mp2_t2[q, q, p, p]/2. if (abs(-mp2_t2[q, q, p, p]/2.) > 1e-15) else 0.
+                     for p, q in itertools.product(range(self.n_virtual), range(self.n_occupied))]
+        # Get doubles amplitudes associated with two spatial occupied-virtual pairs
+        doubles_2 = [-mp2_t2[q, s, p, r] for (p, q), (r, s)
+                     in itertools.combinations(itertools.product(range(self.n_virtual), range(self.n_occupied)), 2)]
+
+        return singles + doubles_1 + doubles_2
