9165e5b9e93014fed97ad940e282b0cec67bb7a8
Ryan <ryan.day@1qbit.com>
2021-04-01 15:23:17 -0700

    implement BK, and prelim on scBK

--- qsdk/toolboxes/qubit_mappings/bravyi_kitaev.py
+++ qsdk/toolboxes/qubit_mappings/bravyi_kitaev.py
@@ -1,11 +1,42 @@
 """Tools for performing Bravyi-Kitaev Transformation, as prescribed via the Fenwick Tree mapping.
 This implementation accommodates mapping of qubit registers where the number of qubits is not a 
-power of two."""
+power of two.
+nb: this is a minimal implementation, just wrapping around current openfermion code (v 1.0.1).
+This wrapper enables future refactoring to utilize our own implementation, as needed."""
 
 import itertools
 import numpy
 
 from openfermion.utils import count_qubits
-from openfermion.ops import InteractionOperator, MajoranaOperator, DiagonalCoulombHamiltonian
-
+from openfermion.transforms import bravyi_kitaev as openfermion_bravyi_kitaev
 from qsdk.toolboxes.operators import FermionOperator, QubitOperator
+
+
+def bravyi_kitaev(fermion,n_qubits):
+    """Execute transformation of FermionOperator to QubitOperator 
+    using the Bravyi-Kitaev transformation. 
+    Important note: there are several implementations of "Bravyi Kitaev"
+    transformation, in both the literature, and historical versions of
+    openfermion. This function executes the transformaton defined in
+    arXiv:quant-ph/0003137. Different versions are not necessarily the 
+    same, and result in undesirable performance.
+    This method is a simple wrapper around openfermion's bravyi_kitaev,
+    but we are forcing the user to pass n_qubits to avoid unexpected
+    behaviour.
+
+    Args:
+        fermion (FermionOperator): input fermionic operator to be transformed.
+        n_qubits (int): number of qubits associated with the operator
+
+    Returns:
+        qubit (QubitOperator): output bravyi-kitaev encoded qubit operator
+    """
+    if not (type(n_qubits) is int):
+        raise TypeError("Number of qubits (n_qubits) must be integer type.")
+    if n_qubits < count_qubits(fermion):
+        raise ValueError("Invalid (too few) number of qubits (n_qubits) for input operator.")
+    
+    qubit = openfermion_bravyi_kitaev(fermion, n_qubits = n_qubits)
+
+    return qubit
+ 
