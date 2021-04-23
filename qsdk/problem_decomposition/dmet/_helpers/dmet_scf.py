96861f008d876c3b20f13527f3ca206f71d84ffa
AlexandreF-1qbit <alexandre.fleury@1qbit.com>
2021-04-23 20:11:59 +0000

    Removed outdated license info + doctring formatting.

--- qsdk/problem_decomposition/dmet/_helpers/dmet_scf.py
+++ qsdk/problem_decomposition/dmet/_helpers/dmet_scf.py
@@ -1,28 +1,12 @@
-#   Copyright 2019 1QBit
-#   
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
 """Perform fragment SCF calculation.
 
 The fragment SCF calculation for DMET calculation
 is done here.
-
 """
 
 from pyscf import gto, scf, ao2mo
 from functools import reduce
 import numpy as np
-import scipy
 
 def dmet_fragment_scf(t_list, two_ele, fock, number_electrons, number_orbitals, guess_orbitals, chemical_potential):
     """Perform SCF calculation.
--- qsdk/problem_decomposition/electron_localization/iao_localization.py
+++ qsdk/problem_decomposition/electron_localization/iao_localization.py
@@ -1,17 +1,3 @@
-#   Copyright 2019 1QBit
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
 """Perform IAO localization.
 
 The orbital localization of the canonical orbitals
@@ -23,7 +9,6 @@ the idea of IAO is to map on minao minimal basis set.
 
 For details, refer to:
 G. Knizia, JCTC 9, 4834-4843 (2013).
-
 """
 
 from pyscf import gto
