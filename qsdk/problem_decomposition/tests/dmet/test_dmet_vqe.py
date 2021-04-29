240293849c9a5c9dec5dfe995704c22d23a116d9
AlexandreF-1qbit <alexandre.fleury@1qbit.com>
2021-04-29 17:46:28 +0000

    Two tests added for DMET resources estimation.

--- qsdk/problem_decomposition/tests/dmet/test_dmet_vqe.py
+++ qsdk/problem_decomposition/tests/dmet/test_dmet_vqe.py
@@ -36,6 +36,71 @@ class DMETVQETest(unittest.TestCase):
 
         self.assertAlmostEqual(energy, -1.9916120594, delta=1e-3)
 
+    def test_h4ring_vqe_jw_ressources(self):
+        """ Resources estimation on H4 ring (JW). """
+
+        mol = gto.Mole()
+        mol.atom = H4_RING
+        mol.basis = "minao"
+        mol.charge = 0
+        mol.spin = 0
+        mol.build()
+
+        opt_dmet = {"molecule": mol,
+                    "fragment_atoms": [1, 1, 1, 1],
+                    "fragment_solvers": "vqe",
+                    "electron_localization": Localization.meta_lowdin,
+                    "verbose": False
+                    }
+
+        ref_resources = [{'qubit_hamiltonian_terms': 15, 
+                          'circuit_width': 4, 
+                          'circuit_gates': 158, 
+                          'circuit_2qubit_gates': 64, 
+                          'circuit_var_gates': 12, 
+                          'vqe_variational_parameters': 2}] * 4
+
+        # Run DMET
+        dmet = DMETProblemDecomposition(opt_dmet)
+        dmet.build()
+        resources = dmet.get_resources()
+
+        self.assertEqual(resources, ref_resources)
+
+    def test_h4ring_vqe_bk_ressources(self):
+        """ Resources estimation on H4 ring (BK). """
+
+        mol = gto.Mole()
+        mol.atom = H4_RING
+        mol.basis = "minao"
+        mol.charge = 0
+        mol.spin = 0
+        mol.build()
+
+        vqe_options = {"qubit_mapping": "bk"}
+
+        opt_dmet = {"molecule": mol,
+                    "fragment_atoms": [1, 1, 1, 1],
+                    "fragment_solvers": "vqe",
+                    "electron_localization": Localization.meta_lowdin,
+                    "solvers_options": vqe_options,
+                    "verbose": False
+                    }
+
+        ref_resources = [{'qubit_hamiltonian_terms': 15, 
+                          'circuit_width': 4, 
+                          'circuit_gates': 107, 
+                          'circuit_2qubit_gates': 46, 
+                          'circuit_var_gates': 12, 
+                          'vqe_variational_parameters': 2}] * 4
+
+        # Run DMET
+        dmet = DMETProblemDecomposition(opt_dmet)
+        dmet.build()
+        resources = dmet.get_resources()
+
+        self.assertEqual(resources, ref_resources)
+
 
 if __name__ == "__main__":
     unittest.main()
