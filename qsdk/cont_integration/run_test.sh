9dc4388bdba383a43008e16c4c2d3e36a1ea76b8
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-05-04 12:36:00 -0700

    Fixing stuff for Jenkins

--- qsdk/cont_integration/run_test.sh
+++ qsdk/cont_integration/run_test.sh
@@ -4,5 +4,7 @@
 #more /root/.bashrc
 #source /root/.bashrc
 #ls -l /root/env/bin
+export PYTHONPATH=$PYTHONPATH:/root/agnostic_simulator:/root/qsdk
+
 cd /root/qsdk/
 /root/env/bin/python3 -m unittest
