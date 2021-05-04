f2ee85ccd589a22d2525e9b1d2660685b4867227
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-05-04 11:50:21 -0700

    Fixing stuff for Jenkins

--- qsdk/cont_integration/run_test.sh
+++ qsdk/cont_integration/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Run unit tests
-more /root/.bashrc
-source /root/.bashrc
-ls -l /root/env/bin
-cd /root/qsdk/qsdk
-python3 -m unittest discover
+#more /root/.bashrc
+#source /root/.bashrc
+#ls -l /root/env/bin
+#cd /root/qsdk/qsdk
+/root/env/bin/python3 -m unittest
