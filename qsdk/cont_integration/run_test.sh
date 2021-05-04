2e37cb5380e7bbaea479e595e25a3e89672f7de8
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-05-04 13:20:41 -0700

    fixing tests

--- qsdk/cont_integration/run_test.sh
+++ qsdk/cont_integration/run_test.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH=$PYTHONPATH:/root/agnostic_simulator:/root/qsdk
 
-cd /root/agnostic_simulator
+cd /root/agnostic_simulator/tests/
 /root/env/bin/python3 -m unittest
 
 cd /root/qsdk/
