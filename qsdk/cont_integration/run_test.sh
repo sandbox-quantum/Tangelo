e755052123a83309503912811f57d73735054f6c
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-05-04 12:54:25 -0700

    Fixing Jenkins tests

--- qsdk/cont_integration/run_test.sh
+++ qsdk/cont_integration/run_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Run unit tests
-#more /root/.bashrc
-#source /root/.bashrc
-#ls -l /root/env/bin
 export PYTHONPATH=$PYTHONPATH:/root/agnostic_simulator:/root/qsdk
+
+cd /root/agnostic_simulator
+/root/env/bin/python3 -m unittest
 
 cd /root/qsdk/
 /root/env/bin/python3 -m unittest
+
+/root/env/bin/pip3 freeze
