5407ade12029e12e897a5ff5657f7eb1347ea47f
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-05-04 13:37:54 -0700

    Cleanup

--- qsdk/cont_integration/run_test.sh
+++ qsdk/cont_integration/run_test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# qsdk and agn sim to be found during tests
 export PYTHONPATH=$PYTHONPATH:/root/agnostic_simulator:/root/qsdk
 
+# Tests agn sim
 cd /root/agnostic_simulator/tests/
 /root/env/bin/python3 -m unittest
 
+# Tests qsdk
 cd /root/qsdk/
 /root/env/bin/python3 -m unittest
 
-/root/env/bin/pip3 freeze
