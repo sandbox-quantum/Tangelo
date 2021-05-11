d97d0e05cb042087544ed949a6c48862aafb28f0
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-05-11 15:16:06 -0700

    Fixing path for automated tests

--- qsdk/qsdk/toolboxes/post_processing/tests/test_mcweeny_purification.py
+++ qsdk/qsdk/toolboxes/post_processing/tests/test_mcweeny_purification.py
@@ -1,7 +1,10 @@
 import unittest
+import os
 import numpy as np
 
 from qsdk.toolboxes.post_processing import mcweeny_purify_2rdm
+
+path_data = os.path.dirname(__file__) + '/data'
 
 
 class McWeenyPurificationTest(unittest.TestCase):
@@ -9,11 +12,11 @@ class McWeenyPurificationTest(unittest.TestCase):
     def test_mcweeny_exact(self):
         """ Check McWeeny's RDM purification technique using exact simulation results.
         Input, output and code as used in https://arxiv.org/pdf/2102.07045.pdf, pot 7 of the H10 curve """
-        with open("data/rdm2_spin_pot7_exact.npy", 'rb') as f:
+        with open(f"{path_data}/rdm2_spin_pot7_exact.npy", 'rb') as f:
             rdm2_spin = np.load(f)
-        with open("data/rdm1_nw_exact.npy", 'rb') as f:
+        with open(f"{path_data}/rdm1_nw_exact.npy", 'rb') as f:
             rdm1_mw_ref = np.load(f)
-        with open("data/rdm2_nw_exact.npy", 'rb') as f:
+        with open(f"{path_data}/rdm2_nw_exact.npy", 'rb') as f:
             rdm2_mw_ref = np.load(f)
 
         rdm1_mw, rdm2_mw = mcweeny_purify_2rdm(rdm2_spin)
@@ -23,11 +26,11 @@ class McWeenyPurificationTest(unittest.TestCase):
     def test_mcweeny_experimental(self):
         """ Check McWeeny's RDM purification technique using experimental simulation results.
         Input, output and code as used in https://arxiv.org/pdf/2102.07045.pdf, pot 7 of the H10 curve """
-        with open("data/rdm2_spin_pot7_experimental.npy", 'rb') as f:
+        with open(f"{path_data}/rdm2_spin_pot7_experimental.npy", 'rb') as f:
             rdm2_spin = np.load(f)
-        with open("data/rdm1_nw.npy", 'rb') as f:
+        with open(f"{path_data}/rdm1_nw.npy", 'rb') as f:
             rdm1_mw_ref = np.load(f)
-        with open("data/rdm2_nw.npy", 'rb') as f:
+        with open(f"{path_data}/rdm2_nw.npy", 'rb') as f:
             rdm2_mw_ref = np.load(f)
 
         rdm1_mw, rdm2_mw = mcweeny_purify_2rdm(rdm2_spin)
