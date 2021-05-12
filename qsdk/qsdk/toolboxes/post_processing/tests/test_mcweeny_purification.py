1d5f1ecb7792d2f0cd3c9178179bb870237b20e3
Valentin Senicourt <valentinsenicourt@Valentins-MacBook-Pro.local>
2021-05-12 12:53:09 -0700

    Added a bit more info to test doc

--- qsdk/qsdk/toolboxes/post_processing/tests/test_mcweeny_purification.py
+++ qsdk/qsdk/toolboxes/post_processing/tests/test_mcweeny_purification.py
@@ -11,7 +11,7 @@ class McWeenyPurificationTest(unittest.TestCase):
 
     def test_mcweeny_exact(self):
         """ Check McWeeny's RDM purification technique using exact simulation results.
-        Input, output and code as used in https://arxiv.org/pdf/2102.07045.pdf, pot 7 of the H10 curve """
+        Input, output and code as used in https://arxiv.org/pdf/2102.07045.pdf, R=1.1 A (pot 7) of the H10 curve """
         with open(f"{path_data}/rdm2_spin_pot7_exact.npy", 'rb') as f:
             rdm2_spin = np.load(f)
         with open(f"{path_data}/rdm1_nw_exact.npy", 'rb') as f:
@@ -25,7 +25,7 @@ class McWeenyPurificationTest(unittest.TestCase):
 
     def test_mcweeny_experimental(self):
         """ Check McWeeny's RDM purification technique using experimental simulation results.
-        Input, output and code as used in https://arxiv.org/pdf/2102.07045.pdf, pot 7 of the H10 curve """
+        Input, output and code as used in https://arxiv.org/pdf/2102.07045.pdf, R=1.1 A (pot 7) of the H10 curve """
         with open(f"{path_data}/rdm2_spin_pot7_experimental.npy", 'rb') as f:
             rdm2_spin = np.load(f)
         with open(f"{path_data}/rdm1_nw.npy", 'rb') as f:
