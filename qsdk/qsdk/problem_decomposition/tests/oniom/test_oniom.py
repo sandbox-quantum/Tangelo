a2dac3a7ddc126039901232a7718a32e938ff310
AlexandreF-1qbit <alexandre.fleury@1qbit.com>
2021-05-14 14:52:33 +0000

    ONIOM+VQE test added.

--- qsdk/qsdk/problem_decomposition/tests/oniom/test_oniom.py
+++ qsdk/qsdk/problem_decomposition/tests/oniom/test_oniom.py
@@ -1,36 +1,54 @@
 import unittest
-import numpy as np
 
-from qsdk.problem_decomposition.oniom.oniom import oniom_model
-from qsdk.problem_decomposition.oniom.utility import build_molecule, load_text, fragment
+from qsdk.problem_decomposition.oniom.oniom_problem_decomposition import ONIOMProblemDecomposition
+from qsdk.problem_decomposition.oniom._helpers.helper_classes import Fragment
 
 
 class ONIOMTest(unittest.TestCase):
 
     def test_vqe_cc(self):
-        atoms = [('H',(0,0,0)), ('H',(0,0,0.75)),
-                ('H',(0,0,2)), ('H',(0,0,2.75))]
+        geometry = [('H', (0., 0., 0.)), ('H', (0., 0., 0.75)),
+                 ('H', (0., 0., 2.)), ('H', (0., 0., 2.75))]
 
-        high_vqe = {'method': 'vqe','basis': 'sto-3g'}
-        high_cc = {'method': 'ccsd','basis': 'sto-3g'}
-        low = {'method': 'rhf','basis': 'sto-3g'}
+        options_both = {"basis": "sto-3g"}
 
-        units = 'Angstrom'
+        # With this line, the interaction between H2-H2 is computed with a low
+        # method.
+        system = Fragment(solver_low="RHF", options_low=options_both)
 
-        system = fragment(solver=low, spin=0, charge=0, units=units)
-        model_vqe_1 = fragment(solver=[high_vqe, low], select_atoms=[0, 1], links=None, spin=0, charge=0, units=units)
-        model_vqe_2 = fragment(solver=[high_vqe, low], select_atoms=[2, 3], links=None, spin=0, charge=0, units=units)
-        model_cc_1 = fragment(solver=[high_cc, low], select_atoms=[0, 1], links=None,spin=0, charge=0, units=units)
-        model_cc_2 = fragment(solver=[high_cc, low], select_atoms=[2, 3], links=None,spin=0, charge=0, units=units)
+        # VQE-UCCSD fragments.
+        model_vqe_1 = Fragment(solver_low="RHF",
+                               options_low=options_both,
+                               solver_high="VQE",
+                               options_high=options_both,
+                               selected_atoms=[0, 1])
+        model_vqe_2 = Fragment(solver_low="RHF",
+                               options_low=options_both,
+                               solver_high="VQE",
+                               options_high=options_both,
+                               selected_atoms=[2, 3])
 
-        oniom_model_vqe = oniom_model(atoms, [system,model_vqe_1, model_vqe_2])
-        e_tot_vqe = oniom_model_vqe.run()
+        # Compairing VQE-UCCSD to CCSD.
+        model_cc_1 = Fragment(solver_low="RHF",
+                              options_low=options_both,
+                              solver_high="CCSD",
+                              options_high=options_both,
+                              selected_atoms=[0, 1])
+        model_cc_2 = Fragment(solver_low="RHF",
+                              options_low=options_both,
+                              solver_high="CCSD",
+                              options_high=options_both,
+                              selected_atoms=[2, 3])
 
-        oniom_model_cc = oniom_model(atoms, [system, model_cc_1, model_cc_2])
-        e_tot_cc = oniom_model_cc.run()
-        self.assertAlmostEqual(e_tot_vqe, e_tot_cc, places=5)
+        oniom_model_vqe = ONIOMProblemDecomposition({"geometry": geometry, "fragments": [system, model_vqe_1, model_vqe_2]})
+        e_oniom_vqe = oniom_model_vqe.simulate()
+
+        oniom_model_cc = ONIOMProblemDecomposition({"geometry": geometry, "fragments": [system, model_cc_1, model_cc_2]})
+        e_oniom_cc = oniom_model_cc.simulate()
+
+        # The two results (VQE-UCCSD and CCSD) should be more or less the same.
+        self.assertAlmostEqual(e_oniom_vqe, e_oniom_cc, places=5)
 
 
 if __name__ == "__main__":
-
     unittest.main()
