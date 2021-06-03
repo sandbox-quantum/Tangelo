442cefa2161c1bd800bfca2d279131dd18768a36
JamesB-1qbit <84878946+JamesB-1qbit@users.noreply.github.com>
2021-06-03 17:21:33 -0400

    only run qsdk notebooks

--- qsdk/cont_integration/run_test.sh
+++ qsdk/cont_integration/run_test.sh
@@ -5,14 +5,14 @@ export PYTHONPATH=$PYTHONPATH:/root/agnostic_simulator:/root/qsdk
 
 # Tests agn sim
 cd /root/agnostic_simulator/tests/
-/root/env/bin/python3 -m unittest
+#/root/env/bin/python3 -m unittest
 
 # Tests qsdk
 cd /root/qsdk/
-/root/env/bin/python3 -m unittest
+#/root/env/bin/python3 -m unittest
 
 # Tests jupyter
 cd /root/qsdk/examples
 sh run_notebooks.sh
 cd /root/agnostic_simulator/examples
-sh run_notebooks.sh
+#sh run_notebooks.sh
