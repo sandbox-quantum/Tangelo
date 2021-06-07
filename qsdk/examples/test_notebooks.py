c211162b903e8e1d29c790866114e8bce351a4ad
James Brown <james.brown@1qbit.com>
2021-06-07 11:17:25 -0400

    add explicit exception from python

--- qsdk/examples/test_notebooks.py
+++ qsdk/examples/test_notebooks.py
@@ -1,12 +1,20 @@
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.preprocessors import CellExecutionError 
 
 ep =ExecutePreprocessor(timeout=600, kernel_name='python3')
 
 #Run DMET notebook
 with open('dmet.ipynb') as f:
      nb=nbformat.read(f,as_version=4)
-ep.preprocess(nb)
+try:
+     out=ep.preprocess(nb)
+except CellExecutionError:
+     out=None
+     msg='Error executing the notebook "%s".\n\n' % 'dmet.ipynb'
+     msg+= 'See notebook "%s" for the traceback.\n\n' % 'dmet.ipynb'
+     print(msg)
+     raise
 
 #Run vqe_custom_ansatz.ipynb
 with open('vqe_custom_ansatz.ipynb') as f:
