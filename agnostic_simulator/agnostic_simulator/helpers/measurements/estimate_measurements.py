dc88d86d7e6a0ed4b45b6816fea26f1cd2fce9cb
ValentinS4t1qbit <41597680+ValentinS4t1qbit@users.noreply.github.com>
2021-06-10 11:42:51 -0700

    Measurements coeffs (#36)
    * Allow for any number of sample to work, by generating samples in chunks that fit the memory of the machine
    * Support for base heuristics estimating number of measurements w.r.t qb operator coefficients
    * Optimal circuit now returned by the default optimizer in VQE. Otherwise can be rebuilt using the ansatz object and optimal parameters.

---
+++ agnostic_simulator/agnostic_simulator/helpers/measurements/estimate_measurements.py
@@ -0,0 +1,43 @@
+"""
+    This file provides functions allowing users to estimate the number of measurements that are needed to
+    approximate the expectation values of some qubit operators up to a given accuracy.
+
+    The functions range from simple general heuristics to more complex approaches that may leverage our knowledge
+    of the quantum state.
+"""
+
+import math
+
+
+def get_measurement_estimate(qb_op, digits=3, method="uniform"):
+    """ Given a qubit operator and a level of accuracy, computes the number of measurements required by each term
+     of the qubit operator to reach the accuracy provided by the user when computing expectation values, returns it
+     as a dictionary mapping measurement basis to number of measurements.
+
+     "uniform" method makes no assumption about the underlying probability distribution resulting from the quantum
+     state preparation circuit. The rule of thumb is "Multiply number of samples by 100 for each digit of accuracy".
+
+    Args:
+        qb_op: qubit operator
+        digits (integer): number of digits of accuracy desired on expectation value
+    Returns:
+        measurements (dict): Dictionary mapping terms / measurement bases to their number of measurements
+    """
+
+    available_methods = {'uniform'}
+    if method not in available_methods:
+        raise NotImplementedError(f"Only available methods are {available_methods}")
+
+    scaling_factor = 100**(digits+1)
+
+    measurements = dict()
+    for term, coef in qb_op.terms.items():
+        coef = max(abs(coef.real), abs(coef.imag))
+        if not term or coef < 10**(-digits):
+            measurements[term] = 0
+        else:
+            n_samples = math.floor(scaling_factor * 100**(math.log10(coef)))
+            # Assign to dictionary, handle edge case
+            measurements[term] = 100 if n_samples == 1 else n_samples
+
+    return measurements
