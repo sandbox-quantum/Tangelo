1da335c023cf7d8924f0ebfadcf3f319f9b5a353
AlexandreF-1qbit <alexandre.fleury@1qbit.com>
2021-06-11 16:15:51 -0400

    Cleaning of oniom_gradients and docstrings.

--- qsdk/qsdk/problem_decomposition/oniom/_helpers/oniom_gradients.py
+++ qsdk/qsdk/problem_decomposition/oniom/_helpers/oniom_gradients.py
@@ -1,35 +1,42 @@
-"""Docstring"""
+"""ONIOM energy derivatives with respect nuclear coordinates. useful for the
+geometry optimization of an ONIOM model.
+"""
 
 import numpy as np
 from pyscf import gto, lib
 
 
 def as_scanner(grad):
-    """Define scanner class specific to gradient method passed in.
-    This relies on the ONIOM procedure's definition of the
-    energy gradient.
-    *args*:
-        - **grad**: instance of oniom grad class, defined below
-    *return*:
-        - **ONIOM_GradScanner(grad)**
+    """Define scanner class specific to gradient method passed in. This relies
+    on the ONIOM procedure's definition of the energy gradient.
+
+    Args:
+        grad (ONIOMGradient): Instance of oniom grad class.
+
+    Returns:
+        ONIOM_GradScanner: Gradient scanner for nucleus energy derivatives.
     """
 
     class ONIOM_GradScanner(grad.__class__, lib.GradScanner):
-        """ONIOM gradient scanner class. """
+        """ONIOM gradient scanner class."""
 
         def __init__(self, g):
-            lib.GradScanner.__init__(self, g)
-            self.verbose = self.oniom_model.verbose
-            #self.stdout = self.oniom_model.stdout
-            #self.max_memory = self.model.max_memory
-            self.unit = "AU" #need to deal with this for geometric optimizer, relaxing need for Bohr units
 
+            lib.GradScanner.__init__(self, g)
+
+            self.verbose = self.oniom_model.verbose
+            self.stdout = self.oniom_model.mol.stdout
+            # Need to deal with this for geometric optimizer, relaxing need for Bohr units.
+            self.unit = "bohr"
+
+            # Getting all scanners for every fragments.
             for fragment in self.oniom_model.fragments:
                 fragment.get_scanners()
 
-        def __call__(self, geometry, **kwargs):
+        def __call__(self, geometry):
 
-            if isinstance(geometry, gto.Mole): #define updated molecular geometry
+            # Updating the molecule geometry.
+            if isinstance(geometry, gto.Mole):
                 mol = geometry
             else:
                 mol = self.oniom_model.mol.set_geom_(geometry, inplace=False)
@@ -40,35 +47,31 @@ def as_scanner(grad):
             # Compute energy and gradient.
             e_tot, de = self.kernel()
 
-            #Update molecule attributes
+            # Update molecule attributes.
             self.oniom_model.mol = mol
             self.mol = mol
 
             return e_tot, de
 
+
     return ONIOM_GradScanner(grad)
 
 
 class ONIOMGradient:
-    """ONIOM gradient class. Each layer has its own solver-specific
-    gradient which, when implemented as a scanner enables fast updating
-    by saving the density-matrix, as well as gradients for SCF fields e.g.
-    At the ONIOM level, we maintain this treatment of layer-specific gradients,
-    and bring each together to define a high-level ONIOM energy gradient.
+    """ONIOM gradient class. Each layer has its own solver-specific gradient which,
+    when implemented as a scanner enables fast updating by saving the density-matrix,
+    as well as gradients for SCF fields e.g. at the ONIOM level, we maintain this
+    treatment of layer-specific gradients, and bring each together to define a
+    high-level ONIOM energy gradient.
     """
 
     def __init__(self, oniom_model):
-        """
-        Initialize gradient object. If each layer does not yet have a
-        gradient_scanner initialized, this is done at the start.
-        *args*:
-            - **model**: instance of oniom_model class
-        """
 
         self.oniom_model = oniom_model
-        self.mol = self.oniom_model.mol #link to model molecule
+        self.mol = self.oniom_model.mol
         self.base = oniom_model
 
+        # Getting all scanners for every fragments if they are not defined yet.
         if not np.product([hasattr(f, "grad_scanners") for f in self.oniom_model.fragments]):
             for fragment in self.oniom_model.fragments:
                 fragment.get_scanners()
@@ -78,20 +81,25 @@ class ONIOMGradient:
         Gradient is defined with respect to atomic coordinates of the system, R = R_SYS.
         dE_ONIOM/dR = dE_LOW[R]/dR + sum_i dE_HIGH_i[R_i]/dR - dE_LOW_i[R_i]/dR
         one can use chain rule to express
+
         d/dR = dR_i/dR d/dR_i = J(R_i,R) d/dR_i
         dE_ONIOM/dR = dE_LOW[R]/dR + sum_i dE_HIGH_i[R_i]/dR_i . J(R_i,R) - dE_LOW_i[R_i]/dR_i.J(R_i,R)
-        Each layer is instantiated with its Jacobian, making this a trivial calculation done below here
-        using the einsum method. This conveniently handles the different # of atoms in models, system,
-        relying on the well-defined functional form of the link-atoms placement to enable proper treatment
-        of all atoms in the system.
-        *return*:
-            - **e_tot**: float, total energy
-            - **de**: numpy array of Nx3 float, with N # of atoms in the system
+
+        Each layer is instantiated with its Jacobian, making this a trivial
+        calculation made below here using the einsum method. This conveniently
+        handles the different number of atoms in models, system, relying on the
+        well-defined functional form of the link-atoms placement to enable proper
+        treatment of all atoms in the system.
+
+        Returns:
+            float, np.array: Total energy and energy derivatives, Nx3 matrix of
+                floats.
         """
 
         e_tot = 0
         de = np.zeros((len(self.oniom_model.geometry), 3))
 
+        # Computing energy and gradients for each fragments.
         for fragment in self.oniom_model.fragments:
 
             grad_scanner_low = fragment.grad_scanners[0]
@@ -104,10 +112,8 @@ class ONIOMGradient:
                 etmp = etmp_high - etmp
                 dtmp = dtmp_high - dtmp
 
-            jacobian = self.oniom_model.get_jacobian(fragment)
-
             e_tot += etmp
-            de += np.einsum('ij,ik->kj', dtmp, jacobian)
+            de += np.einsum('ij,ik->kj', dtmp, fragment.jacobian)
 
         return e_tot, de
 
