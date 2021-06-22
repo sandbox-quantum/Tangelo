de3c520f81a817a22295aec3ad9aa9e5a1d0a2d4
James Brown <james.brown@1qbit.com>
2021-06-22 10:44:37 -0400

    updated hea

--- qsdk/qsdk/toolboxes/ansatz_generator/hea.py
+++ qsdk/qsdk/toolboxes/ansatz_generator/hea.py
@@ -127,6 +127,8 @@ class HEA(Ansatz):
         hea_circuit = HEACircuit(self.n_qubits, self.n_layers, self.rot_type)
 
         if reference_state_circuit.size != 0:
+            print('reference circuit')
+            print(reference_state_circuit)
             self.circuit = reference_state_circuit + hea_circuit
         else:
             self.circuit = hea_circuit
