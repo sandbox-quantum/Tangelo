f9eae2414d3e1c1553cd2471706d6b3775d84869
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2021-07-21 16:18:26 -0400

    Docstrings.

--- qsdk/qsdk/toolboxes/ansatz_generator/adapt_uccgsd.py
+++ qsdk/qsdk/toolboxes/ansatz_generator/adapt_uccgsd.py
@@ -1,18 +1,42 @@
-"""Doc string"""
+"""This module defines the adaptive UCCGSD ansatz class. """
 
 import math
-import numpy as np
 
 from agnostic_simulator import Circuit
 from qsdk.toolboxes.qubit_mappings.statevector_mapping import get_reference_circuit
 from qsdk.toolboxes.ansatz_generator.ansatz_utils import pauliword_to_circuit
 from qsdk.toolboxes.ansatz_generator.ansatz import Ansatz
 from qsdk.toolboxes.qubit_mappings.mapping_transform import fermion_to_qubit_mapping
-from qsdk.toolboxes.ansatz_generator._unitary_cc import uccsd_singlet_generator
+from qsdk.toolboxes.ansatz_generator._general_unitary_cc import uccgsd_generator
 
 
 class ADAPTUCCGSD(Ansatz):
-    """Doc string """
+    """Adaptive ansatz used with ADAPT-VQE. The operator pool is taken from
+    excitation defined with UCCGSD.
+
+    Args:
+        n_spinorbitals (int): Number of spin orbitals in a given basis.
+        n_electrons (int): Number of electrons.
+        operators (list of QubitOperator): List of operator to consider at the
+            construction step. Can be useful for restarting computation.
+        ferm_operators (list of FermionOperator): Same as operators, but in
+            fermionic form. Not necessary for running the ansatz, but it is
+            convenient for analyzing results.
+        mapping (string): Qubit encoding.
+        up_then_down (bool): Ordering convention.
+
+    Attributes:
+        var_params (list of float): Variational parameters.
+        circuit (angostic_simulation.Circuit): Quantum circuit.
+        length_operators (list of int): Length of every self.operators. With an
+            adaptive ansatz, one variational parameter correponds to a single
+            cycle. Every term are hermitian, so there are many terms in each
+            'meta' term. Therefore, many variational gates are defined for each
+            operator in self.operators. This attributes is used to map the number
+            of var_params to actual number of variational gates.
+        var_params_prefactor (list of float): List of 1. or -1. to keep track of
+            each sub-operators sign.
+    """
 
     def __init__(self, n_spinorbitals, n_electrons, operators=list(), ferm_operators=list(), mapping="jw", up_then_down=False):
 
@@ -45,16 +69,19 @@ class ADAPTUCCGSD(Ansatz):
         return len(self.length_operators)
 
     def set_var_params(self, var_params=None):
-        """ Set initial variational parameter values. Defaults to zeros. """
+        """Set initial variational parameter values. Defaults to zeros. """
+
         if var_params is None:
             var_params = [0.]*self.n_var_params
         elif len(var_params) != self.n_var_params:
             raise ValueError('Invalid number of parameters.')
         self.var_params = var_params
+
         return var_params
 
     def update_var_params(self, var_params):
-        """ Update variational parameters (done repeatedly during VQE) """
+        """Update variational parameters (done repeatedly during VQE). """
+
         for var_index in range(self.n_var_params):
             length_op = self.length_operators[var_index] # 2 or 8
 
@@ -65,11 +92,12 @@ class ADAPTUCCGSD(Ansatz):
 
     def prepare_reference_state(self):
         """ Prepare a circuit generating the HF reference state. """
-        # TODO non hardcoded mapping
+
         return get_reference_circuit(n_spinorbitals=self.n_spinorbitals, n_electrons=self.n_electrons, mapping=self.mapping, up_then_down=self.up_then_down)
 
     def build_circuit(self, var_params=None):
-        """ Construct the variational circuit to be used as our ansatz. """
+        """Construct the variational circuit to be used as our ansatz."""
+
         self.set_var_params(var_params)
 
         self.circuit = Circuit(n_qubits=self.n_spinorbitals)
@@ -94,13 +122,23 @@ class ADAPTUCCGSD(Ansatz):
         return self.circuit
 
     def add_operator(self, pauli_operator, ferm_operator=None):
-        """Add a new operator to our circuit"""
+        """Add a new operator to our circuit.
 
+        Args:
+            pauli_operator (QubitOperator): Operator to convert into a circuit and
+                append it to the present circuit.
+            ferm_operator (FermionicOperator): Same operator in fermionic form.
+        """
+
+        # Keeping track of the added operator.
         self.length_operators += [len(pauli_operator.terms)]
         self.operators.append(pauli_operator)
+
         if ferm_operator is not None:
             self.ferm_operators += [ferm_operator]
 
+        # Going through every term and convert them into a circuit. Keeping track
+        # of each term sign.
         for pauli_term in pauli_operator.get_operators():
             coeff = list(pauli_term.terms.values())[0]
             self.var_params_prefactor += [math.copysign(1., coeff)]
@@ -111,23 +149,13 @@ class ADAPTUCCGSD(Ansatz):
             self.circuit += new_operator
 
     def get_pool(self):
-        """TBD
+        """Compute all excitation possible with this ansatz.
+
+        Returns:
+            list of QubitOperator, list of fermionic operator: Self explanatory.
         """
-        # Initialize pool of operators like in ADAPT-VQE paper, based on single and double excitations (Work in progress)
-        # Use uccsd functions from openfermion to get a hold on the single and second excitations, per Lee's advice.
-
-        n_spatial_orbitals = self.n_spinorbitals // 2
-        n_occupied = int(np.ceil(self.n_electrons / 2))
-        n_virtual = n_spatial_orbitals - n_occupied
-        n_singles = n_occupied * n_virtual
-        n_doubles = n_singles * (n_singles + 1) // 2
-        n_amplitudes = n_singles + n_doubles
-
-        f_op = uccsd_singlet_generator([1.]*n_amplitudes, 2*n_spatial_orbitals, self.n_electrons)
-        lst_fermion_op = list()
-        for i in f_op.get_operator_groups(len(f_op.terms) // 2):
-            lst_fermion_op.append(i)
-
+        # Initialize pool of (qubit) operators like in ADAPT-VQE paper, based on single and double excitations.
+        lst_fermion_op = uccgsd_generator(self.n_spinorbitals, up_down=self.up_then_down)
         pool_operators = [fermion_to_qubit_mapping(fermion_operator=fi,
                                                 mapping=self.mapping,
                                                 n_spinorbitals=self.n_spinorbitals,
@@ -138,6 +166,5 @@ class ADAPTUCCGSD(Ansatz):
         for qubit_op in pool_operators:
             for key in qubit_op.terms:
                 qubit_op.terms[key] = math.copysign(1., float(qubit_op.terms[key].imag))
-            qubit_op.compress()
 
         return pool_operators, lst_fermion_op
