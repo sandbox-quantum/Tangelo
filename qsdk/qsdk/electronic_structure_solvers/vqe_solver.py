ea3ce823d18c3f972671a8b25b6ffa4a025cad01
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2021-08-26 09:45:51 -0400

    Change checks for RUCC ansatze consistent with user all user inputs.

--- qsdk/qsdk/electronic_structure_solvers/vqe_solver.py
+++ qsdk/qsdk/electronic_structure_solvers/vqe_solver.py
@@ -134,7 +134,7 @@ class VQESolver:
                 if not self.up_then_down:
                     raise ValueError("Parameter up_then_down must be set to True for {} ansatz.".format(self.ansatz))
                 # Only HOMO-LUMO systems are relevant.
-                if count_qubits(self.qubit_hamiltonian) != 4:
+                if self.qubit_hamiltonian.n_qubits != 4:
                     raise ValueError("The system must be reduced to a HOMO-LUMO problem for {} ansatz.".format(self.ansatz))
 
             # Build / set ansatz circuit. Use user-provided circuit or built-in ansatz depending on user input.
