6b337074ee59eeb57f13bf64b2756a112c0cf4c0
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2021-08-30 14:56:33 -0400

    Added a conversion function for qu_op to qu_ham.

--- qsdk/qsdk/toolboxes/operators/__init__.py
+++ qsdk/qsdk/toolboxes/operators/__init__.py
@@ -1,1 +1,1 @@
-from .operators import FermionOperator, QubitOperator, count_qubits, normal_ordered, squared_normal_ordered, list_to_fermionoperator
+from .operators import FermionOperator, QubitOperator, count_qubits, normal_ordered, squared_normal_ordered, list_to_fermionoperator, qubitop_to_qubitham
