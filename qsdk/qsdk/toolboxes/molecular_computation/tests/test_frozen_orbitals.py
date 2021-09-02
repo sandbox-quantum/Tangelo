39bc76297efdb15813bd9315ca0df25f8705f4c3
James Brown <james.brown@1qbit.com>
2021-09-02 13:28:33 -0400

    changed get_homo_lumo name

--- qsdk/qsdk/toolboxes/molecular_computation/tests/test_frozen_orbitals.py
+++ qsdk/qsdk/toolboxes/molecular_computation/tests/test_frozen_orbitals.py
@@ -2,7 +2,7 @@ import unittest
 from pyscf import gto
 from pyscf.gto.mole import Mole
 
-from qsdk.toolboxes.molecular_computation.frozen_orbitals import get_frozen_core, get_homo_lumo
+from qsdk.toolboxes.molecular_computation.frozen_orbitals import get_frozen_core, get_orbitals_excluding_homo_lumo
 
 H2O_list = [('O', (0., 0., 0.11779)),
             ('H', (0., 0.75545, -0.47116)),
@@ -32,11 +32,11 @@ class FrozenOrbitalsTest(unittest.TestCase):
         """
 
         # Getting HOMO-LUMO.
-        frozen_homo_lumo = get_homo_lumo(mol_h2o)
+        frozen_homo_lumo = get_orbitals_excluding_homo_lumo(mol_h2o)
         self.assertEqual(frozen_homo_lumo, [0, 1, 2, 3, 6, 7, 8, 9, 10, 11, 12])
 
         # Active space from HOMO-2 to LUMO+4
-        frozen_homo2_lumo4 = get_homo_lumo(mol_h2o, 2, 4)
+        frozen_homo2_lumo4 = get_orbitals_excluding_homo_lumo(mol_h2o, 2, 4)
         self.assertEqual(frozen_homo2_lumo4, [0, 1, 10, 11, 12])
 
 
