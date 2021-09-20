93391ccf1a806c14f9ef96378eac33574da88928
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2021-09-20 10:43:24 -0400

    molecule docs.

--- qsdk/qsdk/toolboxes/molecular_computation/tests/test_molecule.py
+++ qsdk/qsdk/toolboxes/molecular_computation/tests/test_molecule.py
@@ -10,34 +10,37 @@ H       0.0        0.0        0.0
 H       0.0        0.0        0.7414
 """
 
-H2O_list = [('O', (0., 0., 0.11779)),
-            ('H', (0., 0.75545, -0.47116)),
-            ('H', (0., -0.75545, -0.47116))
-        ]
+H2O_list = [
+    ("O", (0., 0., 0.11779)),
+    ("H", (0., 0.75545, -0.47116)),
+    ("H", (0., -0.75545, -0.47116))
+]
 
 
 class CoordsTest(unittest.TestCase):
 
     def test_atoms_string_to_list(self):
-        """ Verify conversion from string to list format for atom coordinates """
+        """Verify conversion from string to list format for atom coordinates."""
         assert(atom_string_to_list(H2_string) == H2_list)
 
 
 class SecondQuantizedMoleculeTest(unittest.TestCase):
 
     def test_instantiate_H2(self):
-        """ Verify basic properties of molecule object through instantiation of MolecularData class """
+        """Verify basic properties of molecule object through instantiation of
+        MolecularData class.
+        """
 
         molecule = SecondQuantizedMolecule(H2_list, 0, 0, "sto-3g")
 
-        assert(molecule.elements == ['H']*2)
-        assert(molecule.basis == 'sto-3g')
+        assert(molecule.elements == ["H"]*2)
+        assert(molecule.basis == "sto-3g")
         assert(molecule.spin == 0)
         assert(molecule.q == 0)
         assert(molecule.n_electrons == 2)
 
     def test_freezing_orbitals(self):
-        """ Verify freezing orbitals functionalities """
+        """Verify freezing orbitals functionalities."""
 
         no_freezing = SecondQuantizedMolecule(H2O_list, 0, 0, "sto-3g", frozen_orbitals=None)
         assert(no_freezing.active_occupied == [0, 1, 2, 3, 4])
@@ -58,7 +61,7 @@ class SecondQuantizedMoleculeTest(unittest.TestCase):
         assert(freeze_with_list.frozen_virtual == [6])
 
     def test_freezing_empty(self):
-        """ Verify freezing orbitals empty input """
+        """Verify freezing orbitals empty input."""
 
         # None should result in the same as nothing.
         none_as_frozen = SecondQuantizedMolecule(H2O_list, frozen_orbitals=None)
@@ -75,7 +78,7 @@ class SecondQuantizedMoleculeTest(unittest.TestCase):
         assert(empty_as_frozen.frozen_virtual == [])
 
     def test_freezing_type_exception(self):
-        """ Verify freezing orbitals exceptions """
+        """Verify freezing orbitals exceptions."""
 
         # Cases where the input is wrong type.
         with self.assertRaises(TypeError):
@@ -86,7 +89,7 @@ class SecondQuantizedMoleculeTest(unittest.TestCase):
             SecondQuantizedMolecule(H2O_list, frozen_orbitals=[0, 1, 2.2222, 3, 4, 5])
 
     def test_no_active_electron(self):
-        """ Verify if freezing all active orbitals fails """
+        """Verify if freezing all active orbitals fails."""
 
         # Cases where no active electron are remaining.
         with self.assertRaises(ValueError):
@@ -95,14 +98,14 @@ class SecondQuantizedMoleculeTest(unittest.TestCase):
             SecondQuantizedMolecule(H2O_list, frozen_orbitals=[0, 1, 2, 3, 4, 5])
 
     def test_no_active_virtual(self):
-        """ Verify if freezing all virtual orbitals fails """
+        """Verify if freezing all virtual orbitals fails."""
 
         # Cases where no active virtual orbitals are remaining.
         with self.assertRaises(ValueError):
             SecondQuantizedMolecule(H2O_list, frozen_orbitals=[5, 6])
 
     def test_get_fermionic_hamiltonian(self):
-        """ Verify energy shift in molecular hamiltonian """
+        """Verify energy shift in molecular hamiltonian."""
 
         molecule = SecondQuantizedMolecule(H2O_list, frozen_orbitals=1)
         shift = molecule.fermionic_hamiltonian.constant
