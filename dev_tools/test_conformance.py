f2107fffa1ea71b546598708fcab5d436ee2cf8b
James Brown <james.brown@1qbit.com>
2021-10-04 12:19:26 -0400

    added unittest.main()

--- dev_tools/test_conformance.py
+++ dev_tools/test_conformance.py
@@ -8,3 +8,7 @@ class TestCodeFormat(unittest.TestCase):
         style = pycodestyle.StyleGuide(quiet=False, config_file="pycodestyle")
         result = style.check_files(["../qsdk"])
         self.assertEqual(result.total_errors, 0, "Found code style errors and warnings.")
+
+
+if __name__ == "__main__":
+    unittest.main()
