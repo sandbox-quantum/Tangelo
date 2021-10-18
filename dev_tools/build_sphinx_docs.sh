1d8a9ccc71e2cad214f18855e7e104a6385b7d50
ValentinS4t1qbit <41597680+ValentinS4t1qbit@users.noreply.github.com>
2021-10-18 10:21:00 -0700

    Smol fixes

--- dev_tools/build_sphinx_docs.sh
+++ dev_tools/build_sphinx_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../docs
+cd ../docs || cd docs
 pip install sphinx sphinx_rtd_theme nbsphinx
 sudo apt-get install -y latexmk texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended pandoc dvipng
 make clean; make html
