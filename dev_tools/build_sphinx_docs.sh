97b8652575bb67993685fdfbb786186dedcb4c2d
ValentinS4t1qbit <41597680+ValentinS4t1qbit@users.noreply.github.com>
2021-10-18 10:14:03 -0700

    Refactor sphinx build, isolating script to help build locally too

---
+++ dev_tools/build_sphinx_docs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../docs
+pip install sphinx sphinx_rtd_theme nbsphinx
+sudo apt-get install -y latexmk texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended pandoc dvipng
+make clean; make html
