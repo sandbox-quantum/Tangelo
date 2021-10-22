e484568bb634e8133612d92b93638743d6d6093a
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2021-10-22 16:14:02 -0400

    Removing systematic Preparing Q# environment...

--- qsdk/helpers/utils.py
+++ qsdk/helpers/utils.py
@@ -17,10 +17,26 @@
     for the purpose of running / skipping tests, and setting a default simulator.
 """
 
+import os, sys
+
+
+class HiddenPrints:
+    """Class to hide terminal printing with a 'with' block."""
+
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, "w")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
 
 def is_package_installed(package_name):
     try:
-        exec(f'import {package_name}')
+        # Hiding prints removes the Preparing Q# envrionement...
+        with HiddenPrints():
+            exec(f"import {package_name}")
         # DEBUG print(f'{package_name:16s} :: found')
         return True
     except ModuleNotFoundError:
