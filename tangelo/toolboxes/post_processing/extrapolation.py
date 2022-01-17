6034260f048c1486d1c5324e4e7528d3a15d5ceb
Krzysztof Bieniasz <krzysztof.bieniasz@1qbit.com>
2022-01-17 20:13:25 +0100

    New revision of the extrapolation code. Moved common diis and Richardson code to separate function. Implemented explicit Richardson solution. Added the Richardson with exponent estimation.

--- tangelo/toolboxes/post_processing/extrapolation.py
+++ tangelo/toolboxes/post_processing/extrapolation.py
@@ -13,12 +13,13 @@
 # limitations under the License.
 
 import numpy as np
-from scipy.optimize import minimize, root
+import scipy.optimize as sp
 
 
 def diis(energies, coeffs):
     """
-    DIIS extrapolation
+    DIIS extrapolation, originally developped by Pulay in
+    Chemical Physics Letters 73, 393-398 (1980)
 
     Args:
         energies (array-like): Energy expectation values for amplified noise rates
@@ -27,22 +28,12 @@ def diis(energies, coeffs):
     Returns:
         float: Extrapolated energy
     """
-    n = len(coeffs)
-    Eh = np.array(energies)
-    ck = np.array(coeffs)
-    B = np.ones((n+1, n+1))
-    B[n, n] = 0
-    B[:n, :n] = ck[:, None] @ ck[None, :]
-    b = np.zeros(n+1)
-    b[n] = 1
-    x = np.linalg.lstsq(B, b, rcond=None)[0]
-    x = x[:-1]
-    return np.dot(Eh, x)
+    return extrapolation(energies, coeffs, 1)
 
 
-def richardson(energies, coeffs):
+def richardson(energies, coeffs, estimate_exp=False):
     """
-    Richardson extrapolation as found in
+    General, DIIS-like extrapolation procedure as found in
     Nature 567, 491-495 (2019) [arXiv:1805.04492]
 
     Args:
@@ -52,14 +43,102 @@ def richardson(energies, coeffs):
     Returns:
         float: Extrapolated energy
     """
+    if estimate_exp==False:
+        return richardson_analytical(energies, coeffs)
+    else:
+        return richardson_with_exp_estimation(energies, coeffs)
+
+
+def extrapolation(energies, coeffs, N=None):
+    """
+    General, DIIS-like extrapolation procedure as found in
+    Nature 567, 491-495 (2019) [arXiv:1805.04492]
+
+    Args:
+        energies (array-like): Energy expectation values for amplified noise rates
+        coeffs (array-like): Noise rate amplification factors
+        N (int): Taylor expanion order; N=None for Richardson extrapolation (order determined from number of datapoints), N=1 for DIIS extrapolation
+
+    Returns:
+        float: Extrapolated energy
+    """
     n = len(coeffs)
+    if N == None:
+        N = n-1
     Eh = np.array(energies)
-    ck = np.array([coeffs**k for k in range(1, n+1)]).sum(axis=0)
+    ck = np.array(coeffs)
+    ck = np.array([ck**k for k in range(1, N+1)])
     B = np.ones((n+1, n+1))
     B[n, n] = 0
-    B[:n, :n] = ck[:, None] @ ck[None, :]
+    B[:n, :n] = ck.T @ ck
     b = np.zeros(n+1)
     b[n] = 1
     x = np.linalg.lstsq(B, b, rcond=None)[0]
     x = x[:-1]
     return np.dot(Eh, x)
+
+
+def richardson_analytical(energies, coeffs):
+    """
+    Richardson extrapolation exlicit result as found in
+    Phys. Rev. Lett. 119, 180509 [arXiv:1612.02058]
+
+    Args:
+        energies (array-like): Energy expectation values for amplified noise rates
+        coeffs (array-like): Noise rate amplification factors
+
+    Returns:
+        float: Extrapolated energy
+    """
+    Eh = np.array(energies)
+    ck = np.array(coeffs)
+    x = np.array([np.prod((ai:=np.delete(ck, i))/(a - ai)) for i, a in enumerate(ck)])
+    return np.dot(Eh, x)
+
+
+def richardson_with_exp_estimation(energies, coeffs):
+    """
+    Richardson extrapolation by recurrence, with exponent estimation
+
+    Args:
+        energies (array-like): Energy expectation values for amplified noise rates
+        coeffs (array-like): Noise rate amplification factors
+
+    Returns:
+        float: Extrapolated energy
+    """
+    n = len(coeffs)
+    p = 1
+    Eh = np.array(energies)
+    c = np.array(coeffs)
+    ck = np.array(coeffs)
+    p_old = 0
+    dp = 0
+    for i in range(n-1):
+        ti = ck[0]/ck[1]
+        si = ck[0]/ck[2]
+        if ((n > 2) & (i < (n-2))):
+            def f(k):
+                tk = np.sign(ti)*np.abs(ti)**k
+                sk = np.sign(si)*np.abs(si)**k
+                A1 = (tk*Eh[1] - Eh[0])/(tk - 1)
+                A2 = (sk*Eh[2] - Eh[0])/(sk - 1)
+                return (A1 - A2)**2
+            p = sp.minimize(f, p+1, method='BFGS', options={'disp':False}).x[0]
+            if (i == 0):
+                ck = c**p
+        else:
+            break
+        for j in range(n-i-1):
+            ti = (ck[j]/ck[j+1])
+            if (i > 0):
+                dp = p - p_old
+                if (dp == 0):
+                    break
+                ck[j] = ck[j]*(c[j]**dp - c[j+1]**dp)/(ti - 1)
+                ti = (ck[j]/ck[j+1])
+            else:
+                ck[j] = ck[j]*(c[j] - c[j+1])/(ti - 1)
+            Eh[j] = (ti*Eh[j+1] - Eh[j])/(ti - 1)
+        p_old = p
+    return(Eh[0])
--- tangelo/toolboxes/post_processing/tests/test_extrapolation.py
+++ tangelo/toolboxes/post_processing/tests/test_extrapolation.py
@@ -13,39 +13,28 @@
 # limitations under the License.
 
 import unittest
-import os
 import numpy as np
 
 from tangelo.toolboxes.post_processing import diis, richardson
 
-path_data = os.path.dirname(os.path.abspath(__file__)) + "/data"
-
+energies = [-1.0477557352266311, -1.0430228852530665, -1.0336456812527268, -1.0300524502477852]
+coeffs = [1., 1.1, 1.2, 1.3]
 
 class ExtrapolationTest(unittest.TestCase):
 
     def test_diis(self):
-        """Test DIIS extrapolation on small sample data from Alejandro
+        """Test DIIS extrapolation on small sample data
         """
-        with open(f"{path_data}/diis_test.dat") as f:
-            data = np.loadtxt(f)
-            coeffs = data[:-1, 0]
-            energies = data[:-1, 1]
-            reference = data[-1, 1]
-
         calculated = diis(energies, coeffs)
-        self.assertAlmostEqual(reference, calculated, delta=1e-10)
+        self.assertAlmostEqual(-1.1104793057724596, calculated, delta=1e-10)
 
     def test_richardson(self):
-        """Test Richardson extrapolation on small sample data from Alejandro
+        """Test Richardson extrapolation on small sample data
         """
-        with open(f"{path_data}/richardson_test.dat") as f:
-            data = np.loadtxt(f)
-            coeffs = data[:-1, 0]
-            energies = data[:-1, 1]
-            reference = data[-1, 1]
-
         calculated = richardson(energies, coeffs)
-        self.assertAlmostEqual(reference, calculated, delta=1e-10)
+        self.assertAlmostEqual(-1.4545871813885753, calculated, delta=1e-10)
+        calculated = richardson(energies, coeffs, estimate_exp=True)
+        self.assertAlmostEqual(-1.056015997770032, calculated, delta=1e-10)
 
 
 if __name__ == "__main__":
