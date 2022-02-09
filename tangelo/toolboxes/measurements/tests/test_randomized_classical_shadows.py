0dd08a7f56937e07517fa23dc807944debc004f8
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2022-02-09 09:32:19 -0500

    Added check tests.

--- tangelo/toolboxes/measurements/tests/test_randomized_classical_shadows.py
+++ tangelo/toolboxes/measurements/tests/test_randomized_classical_shadows.py
@@ -51,7 +51,14 @@ class RandomizedClassicalShadowTest(unittest.TestCase):
     def test_initialization(self):
         """Testing the initialization."""
 
-        RandomizedClassicalShadow(state, bitstrings, unitaries, shuffle=False)
+        RandomizedClassicalShadow(state, bitstrings, unitaries)
+
+    def test_same_length_check(self):
+        """Testing the case where arguments are not the same length."""
+
+        wrong_bitstrings = bitstrings + ["00"]
+        with self.assertRaises(AssertionError):
+            RandomizedClassicalShadow(state, wrong_bitstrings, unitaries)
 
     def test_shadow_properties(self):
         """Testing of the shadow properties."""
