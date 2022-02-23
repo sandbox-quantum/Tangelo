b487b9e42964414dcda3f0b8313d154d1a9eefbe
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2022-02-23 17:00:12 -0500

    Fixed get_operators bug + added a test.

--- tangelo/toolboxes/operators/tests/test_operators.py
+++ tangelo/toolboxes/operators/tests/test_operators.py
@@ -22,10 +22,10 @@ class OperatorsUtilitiesTest(unittest.TestCase):
     def test_count_qubits(self):
         """Test count_qubits function."""
 
-        n_qubits_3 = count_qubits(QubitHamiltonian("JW", True, term="X0 Y1 Z2"))
+        n_qubits_3 = count_qubits(QubitHamiltonian("X0 Y1 Z2", mapping="JW", up_then_down=True))
         self.assertEqual(n_qubits_3, 3)
 
-        n_qubits_5 = count_qubits(QubitHamiltonian("JW", True, term="X0 Y1 Z4"))
+        n_qubits_5 = count_qubits(QubitHamiltonian("X0 Y1 Z4", mapping="JW", up_then_down=True))
         self.assertEqual(n_qubits_5, 5)
 
     def test_qubitop_to_qubitham(self):
@@ -44,7 +44,7 @@ class QubitHamiltonianTest(unittest.TestCase):
     def test_instantiate_QubitHamiltonian(self):
         """Test initialization of QubitHamiltonian class."""
 
-        qubit_ham = 0.5 * QubitHamiltonian("BK", False, term="X0 Y1 Z2")
+        qubit_ham = 0.5 * QubitHamiltonian("X0 Y1 Z2", mapping="BK", up_then_down=False)
         reference_attributes = {"terms": {((0, 'X'), (1, 'Y'), (2, 'Z')): 0.5},
                                 "mapping": "BK", "up_then_down": False}
 
@@ -55,21 +55,33 @@ class QubitHamiltonianTest(unittest.TestCase):
         together.
         """
 
-        qubit_ham = QubitHamiltonian("JW", True, term="X0 Y1 Z2")
+        qubit_ham = QubitHamiltonian("X0 Y1 Z2", mapping="JW", up_then_down=True)
 
         with self.assertRaises(RuntimeError):
-            qubit_ham + QubitHamiltonian("BK", True, term="Z0 X1 Y2")
+            qubit_ham + QubitHamiltonian("Z0 X1 Y2", mapping="BK", up_then_down=True)
 
         with self.assertRaises(RuntimeError):
-            qubit_ham + QubitHamiltonian("JW", False, term="Z0 X1 Y2")
+            qubit_ham + QubitHamiltonian("Z0 X1 Y2", mapping="JW",  up_then_down=False)
 
     def test_to_qubit_operator(self):
         """Test exportation of QubitHamiltonian to QubitOperator."""
 
-        qubit_ham = 1. * QubitHamiltonian("JW", True, term="X0 Y1 Z2")
+        qubit_ham = 1. * QubitHamiltonian("X0 Y1 Z2", mapping="JW",  up_then_down=True)
 
         self.assertEqual(qubit_ham.to_qubitoperator(),
             QubitOperator(term="X0 Y1 Z2", coefficient=1.))
+
+    def test_get_operators(self):
+        """Test exportation of QubitHamiltonian to QubitOperator."""
+
+        terms = [QubitHamiltonian("X0 Y1 Z2", mapping="JW", up_then_down=True),
+                 QubitHamiltonian("Z0 X1 Y2", mapping="JW", up_then_down=True),
+                 QubitHamiltonian("Y0 Z1 X2", mapping="JW", up_then_down=True)]
+
+        H = terms[0] + terms[1] + terms[2]
+
+        for ref_term, term in zip(terms, H.get_operators()):
+            self.assertEqual(ref_term, term)
 
 
 if __name__ == "__main__":
