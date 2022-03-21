61b2a75da4ed8976d227671f383f8f891c2610b0
JamesB-1qbit <84878946+JamesB-1qbit@users.noreply.github.com>
2022-03-21 18:21:24 -0400

    Givens gate (#135)
    * added givens gate decomposition
    
    Co-authored-by: ValentinS4t1qbit <41597680+ValentinS4t1qbit@users.noreply.github.com>

--- tangelo/toolboxes/ansatz_generator/ansatz_utils.py
+++ tangelo/toolboxes/ansatz_generator/ansatz_utils.py
@@ -394,3 +394,26 @@ def derangement_circuit(qubit_list, control=None, n_qubits=None, decomp=None):
                                        control=control)]
 
     return Circuit(gate_list, n_qubits=n_qubits)
+
+
+def givens_gate(target, theta, is_variational=False):
+    """Generates the list of gates corresponding to a givens rotation exp(-theta*(XX+YY))
+
+    Explicitly the two-qubit matrix is
+    [[0,      0,           0,       0],
+     [0,  cos(theta), -sin(theta),  0],
+     [0,  sin(theta),  cos(theta),  0],
+     [0,      0,            0,      0]]
+
+    Args:
+        target (list): list of two integers that indicate which qubits are involved in the givens rotation
+        theta (float): the rotation angle
+        is_variational (bool): Whether the rotation angle is a variational parameter.
+
+    Returns:
+        list of Gate: The list of gates corresponding to the givens rotation"""
+    if len(target) != 2:
+        raise ValueError("target must be a list or array of two integers")
+    return [Gate("CNOT", target=target[0], control=target[1]),
+            Gate("CRY", target=target[1], control=target[0], parameter=-theta, is_variational=is_variational),
+            Gate("CNOT", target=target[0], control=target[1])]
