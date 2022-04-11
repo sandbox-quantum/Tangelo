d46d5cd4916569a32d3d47f6a4db81041123af59
James Brown <james.brown@1qbit.com>
2022-04-11 11:34:01 -0400

    sa_vqe_solver from sa-oo-vqe branch

--- tangelo/algorithms/__init__.py
+++ tangelo/algorithms/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .variational import BuiltInAnsatze, VQESolver, ADAPTSolver
+from .variational import BuiltInAnsatze, VQESolver, ADAPTSolver, SA_VQESolver
 from .classical import FCISolver, CCSDSolver, MINDO3Solver
 from .projective import QITESolver
