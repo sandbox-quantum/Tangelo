d6b9f924dc2b4ea335c98401a08a2c70ce5361b3
elloyd-1qbit <58313607+elloyd-1qbit@users.noreply.github.com>
2022-04-13 15:22:40 -0700

    updated docstring and renamed ambiguous variables

--- tangelo/toolboxes/optimizers/rotosolve.py
+++ tangelo/toolboxes/optimizers/rotosolve.py
@@ -15,7 +15,7 @@
 import numpy as np
 
 
-def rotosolve_step(func, var_params, i, *args):
+def rotosolve_step(func, var_params, i, *func_args):
     """Gradient free optimization step using specific points to
     characterize objective function w.r.t to parameter values. Based on
     formulas in arXiv:1905.09692, Mateusz Ostaszewski
@@ -26,19 +26,20 @@ def rotosolve_step(func, var_params, i, *args):
                 and returns a float.
             var_params (list of float): The variational parameters.
             i (int): Index of the variational parameter to update.
+            *func_args (tuple): Optional arguments to pass to func.
         Returns:
             list of floats: Optimal parameters.
     """
 
     # Charaterize sinusoid of objective function using specific parameters
     var_params[i] = 0
-    m_1 = func(var_params, *args)
+    m_1 = func(var_params, *func_args)
 
     var_params[i] = 0.5 * np.pi
-    m_2 = func(var_params, *args)
+    m_2 = func(var_params, *func_args)
 
     var_params[i] = -0.5 * np.pi
-    m_3 = func(var_params, *args)
+    m_3 = func(var_params, *func_args)
 
     # Calculate theta_min based on measured values
     theta_min = -0.5 * np.pi - np.arctan2(2. * m_1 - m_2 - m_3, m_2 - m_3)
@@ -54,7 +55,7 @@ def rotosolve_step(func, var_params, i, *args):
     return var_params
 
 
-def rotosolve(func, var_params, *args, ftol=1e-5, maxiter=100):
+def rotosolve(func, var_params, *func_args, ftol=1e-5, maxiter=100):
     """Optimization procedure for parameterized quantum circuits whose
      objective function varies sinusoidally with the parameters. Based
      on the work by arXiv:1905.09692, Mateusz Ostaszewski.
@@ -66,18 +67,19 @@ def rotosolve(func, var_params, *args, ftol=1e-5, maxiter=100):
         var_params (list): The variational parameters.
         ftol (float): Convergence threshold.
         maxiter (int): The maximum number of iterations.
+        *func_args (tuple): Optional arguments to pass to func.
 
     Returns:
         float: The optimal energy found by the optimizer.
         list of floats: Optimal parameters.
      """
     # Get intial value, and run rotosolve for up to maxiter iterations
-    energy_old = func(var_params, *args)
+    energy_old = func(var_params, *func_args)
     for it in range(maxiter):
         # Update parameters one at a time using rotosolve_step
         for i in range(len(var_params)):
-            var_params = rotosolve_step(func, var_params, i, *args)
-        energy_new = func(var_params, *args)
+            var_params = rotosolve_step(func, var_params, i, *func_args)
+        energy_new = func(var_params, *func_args)
 
         # Check if convergence tolerance is met
         if abs(energy_new - energy_old) <= ftol:
