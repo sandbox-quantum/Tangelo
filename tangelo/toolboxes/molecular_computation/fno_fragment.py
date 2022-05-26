d0a1fb5f19d5a704dab50781d93ae988b7d4a12f
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2022-05-26 11:25:20 -0400

    Working state of getting fermops.

--- tangelo/toolboxes/molecular_computation/fno_fragment.py
+++ tangelo/toolboxes/molecular_computation/fno_fragment.py
@@ -12,77 +12,139 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Docstring.
+"""This file provides functions allowing users to import problem decomposition
+fragment data from a classical calculation. Those fragments could be inputs to
+a quantum computing experiment for quantum chemistry.
 """
 
 import os
 import requests
 
+import h5py
+import numpy as np
 import pandas as pd
 
 
 class QEMIST_FNO_fragment():
+    """Python object to post-process, fetch and manipulate QEMIST Cloud MI-FNO
+    results. The use-case for this is to map MI-FNO subproblems into
+    fermionic Hamiltonians acting as inputs. This object keeps track of the
+    classical results.
+
+    Attributes:
+        e_tot (float): Total MI-FNO energy.
+        e_corr (float): Correlation energy (e_tot - e_mf).
+        e_mf (float): Mean-field energy.
+        frag_info (dict): Information about each fragment. The keys are related
+            to the sampled active space (e.g. '(1,)' or '(0, 2)'). It contains
+            informations about the correction term, epsilon, list of truncated
+            orbitals and more.
+
+    Properties:
+        dataframe (pandas.DataFrame): Converted frag_info dict into a pandas
+            DataFrame.
+    """
 
     def __init__(self, result):
-        """Docstring."""
+        """Initialization method to process the classical results.
 
+        Args:
+            results (dict): Classical computation results (QEMIST Cloud output).
+        """
+
+        # IncrementalDecompositon quantities.
         self.e_tot = result["energy_total"]
         self.e_corr = result["energy_correlation"]
         self.e_mf = result["mean_field_energy"]
 
-        frag_info = {
-            "n_body": list(),
-            "frag_id": list(), "epsilon": list(), "mp2_correlation": list(),
-            "correction": list(), "frozen_orbitals_truncated": list()
-        }
-        for n_body, fragments_per_truncation in result["subproblem_data"].items():
+        relevant_info = {
+            "energy_total",
+            "energy_correlation",
+            "epsilon",
+            "mp2_correlation",
+            "correction",
+            "frozen_orbitals_truncated",
+            "mo_coefficients"
+         }
+
+        # Selecting only the FNO keys found in 'relevant_info'.
+        self.frag_info = {}
+        for _, fragments_per_truncation in result["subproblem_data"].items():
             for frag_id, frag_result in fragments_per_truncation.items():
-
-                frag_info["n_body"] += [n_body]
-                frag_info["frag_id"] += [frag_id]
-                frag_info["epsilon"] += [frag_result.get("epsilon", 0.)]
-                frag_info["mp2_correlation"] += [frag_result.get("mp2_correlation", 0.)]
-                frag_info["correction"] += [frag_result.get("correction", 0.)]
-
-                frag_info["frozen_orbitals_truncated"] += [frag_result.get("frozen_orbitals_truncated", None)]
-                frag_info["mo_coefficients"] += [frag_result.get("mo_coefficients", None)]
-
-        self.frag_info = frag_info
+                self.frag_info[frag_id] = {k: frag_result.get(k, None) for k in relevant_info}
 
     @property
-    def get_frag_info(self):
-        """Docstring."""
+    def dataframe(self):
+        """Outputting the fragment informations as a pandas.DataFrame."""
+        return pd.DataFrame.from_dict(self.frag_info, orient="index")
 
-        data = {k: v for k, v in self.frag_info.items() if k not in {"frozen_orbitals_truncated"}}
-        return pd.DataFrame.from_dict(data, orient="columns")
+    def get_mo_coeff(self, download_path=os.path.expanduser("~")):
+        """Function to fetch molecular orbital coefficients. A download path can
+        be provided to change the directory where the files will be downloaded.
+        If the files already exist, the function skips the download step. The
+        array is stored in the _frag_info[frag_id]["mo_coefficients"]["array"]
+        attribute.
 
-    def download_mo_coeff(self, download_path=os.getcwd()):
-        """Docstring."""
-
+        Args:
+            download_path (string): Path where to download the HDF5 files
+                containing the molecular coefficient array. Default is set to
+                the user's HOME directory.
+        """
         absolute_path = os.path.abspath(download_path)
 
-        for mo_coeff in self.frag_info["mo_coefficients"]:
-            response = requests.get(mo_coeff["s3_url"])
-            file_path = os.path.join(absolute_path, mo_coeff["key"])
+        # For each fragment, fetch the molecular orbital coefficients from the
+        # HDF5 files.
+        for frag_id, frag in self.frag_info.items():
+            file_path = os.path.join(absolute_path, frag["mo_coefficients"]["key"] + ".hdf5")
 
-            with open(file_path, "wb") as file:
-                file.write(response.content)
+            if not os.path.exists(file_path):
+                response = requests.get(frag["mo_coefficients"]["s3_url"])
+
+                with open(file_path, "wb") as file:
+                    file.write(response.content)
+
+            with h5py.File(file_path, "r") as file:
+                mo_coeff = np.array(file["mo_coefficients"])
+
+            self.frag_info[frag_id]["mo_coefficients"]["array"] = mo_coeff
+
+    def get_fermionoperator(self, molecule, frag_id):
+
+        mo_coeff = self.frag_info[frag_id]["mo_coefficients"]["array"]
+        frozen_orbitals = self.frag_info[frag_id]["frozen_orbitals_truncated"]
+
+        # Something is wrong if the molecule provided does not have the same
+        # mean-field energy.
+        assert round(molecule.mf_energy, 6) == round(self.e_mf, 6)
+
+        # Returning a new molecule with the right frozen orbital.
+        new_molecule = molecule.freeze_mos(frozen_orbitals, inplace=False)
+
+        return new_molecule._get_fermionic_hamiltonian(mo_coeff)
 
 
 if __name__ == "__main__":
-    res = {"energy_correlation":-0.06749160267353815,"energy_total":-15.834543249968815,"mean_field_energy":-15.790965945526066,
-    "subproblem_data":{
-        "1":{
-            "(0,)":{"energy_correlation":-0.00005561322528357948,"energy_total":-15.76710726052056,"epsilon":-0.00005561322528357948, "problem_handle":28268144955910096},
-            "(1,)":{"energy_correlation":-0.02942361607204269,"energy_total":-15.79647526336732,"epsilon":-0.02942361607204269, "problem_handle":14851925548623824},
-            "(2,)":{"energy_correlation":-0.029423623773858765,"energy_total":-15.796475271069136,"epsilon":-0.029423623773858765}
-            },
-        "2":{
-            "(0, 1)":{"energy_correlation":-0.031521801844350605,"energy_total":-15.798573449139628,"epsilon":-0.002042572547024335,"problem_handle":23162657201612800},
-            "(0, 2)":{"energy_correlation":-0.03152180146817507,"energy_total":-15.798573448763452,"epsilon":-0.002042564469032726,"problem_handle":28952788512696320},
-            "(1, 2)":{"energy_correlation":-0.06335085243219751,"energy_total":-15.830402499727477,"epsilon":-0.004503612586296057,"problem_handle":20545643433846784}
-            }
-    }
-    }
+    import pickle
+    from tangelo import SecondQuantizedMolecule
+
+    with open("/home/alex/scratch/BeH2_MIFNO_trunc1_result.pkl", "rb") as f:
+        res = pickle.load(f)
+
     test = QEMIST_FNO_fragment(res)
-    print(test.get_frag_info)
+    print(test.dataframe)
+    print(test.e_tot)
+    print(test.e_corr)
+    print(test.e_mf)
+
+    test.get_mo_coeff("/home/alex/scratch/mo_coeff")
+
+    BeH2 = f"""
+        Be   0.0000    0.0000    0.0000
+        H    0.0000    0.0000    1.3264
+        H    0.0000    0.0000   -1.3264
+    """
+    mol = SecondQuantizedMolecule(BeH2, basis="3-21G")
+    #print(mol.__dict__)
+
+    ferm_op = test.get_fermionoperator(mol, "(1,)")
+    print(ferm_op)
--- tangelo/toolboxes/molecular_computation/molecule.py
+++ tangelo/toolboxes/molecular_computation/molecule.py
@@ -295,7 +295,7 @@ class SecondQuantizedMolecule(Molecule):
         self.n_mos = molecule.nao_nr()
         self.n_sos = 2*self.n_mos
 
-    def _get_fermionic_hamiltonian(self):
+    def _get_fermionic_hamiltonian(self, mo_coeff=None):
         """This method returns the fermionic hamiltonian. It written to take
         into account calls for this function is without argument, and attributes
         are parsed into it.
@@ -304,7 +304,7 @@ class SecondQuantizedMolecule(Molecule):
             FermionOperator: Self-explanatory.
         """
 
-        core_constant, one_body_integrals, two_body_integrals = self.get_active_space_integrals()
+        core_constant, one_body_integrals, two_body_integrals = self.get_active_space_integrals(mo_coeff)
 
         one_body_coefficients, two_body_coefficients = spinorb_from_spatial(one_body_integrals, two_body_integrals)
 
@@ -425,7 +425,7 @@ class SecondQuantizedMolecule(Molecule):
 
         return e.real
 
-    def get_active_space_integrals(self):
+    def get_active_space_integrals(self, mo_coeff=None):
         """Computes core constant, one_body, and two-body coefficients with frozen orbitals folded into one-body coefficients
         and core constant
 
@@ -433,7 +433,7 @@ class SecondQuantizedMolecule(Molecule):
             (float, array, array): (core_constant, one_body coefficients, two_body coefficients)
         """
 
-        return self.get_integrals()
+        return self.get_integrals(mo_coeff, True)
 
     def get_full_space_integrals(self, mo_coeff=None):
         """Computes core constant, one_body, and two-body integrals for all orbitals
@@ -442,7 +442,7 @@ class SecondQuantizedMolecule(Molecule):
             (float, array, array): (core_constant, one_body coefficients, two_body coefficients)
         """
 
-        return self.get_integrals(mo_coeff, consider_frozen=False)
+        return self.get_integrals(mo_coeff, False)
 
     def get_integrals(self, mo_coeff=None, consider_frozen=True):
         """Computes core constant, one_body, and two-body coefficients for a given active space and mo_coeff
