96c1d2f038d6af6087aa95b80f7696aba2847116
James Brown <84878946+JamesB-1qbit@users.noreply.github.com>
2022-07-28 13:21:12 -0400

    Fix scbk reference state (#176)
    * fix for scbk ref state and test that previously failed

--- tangelo/toolboxes/qubit_mappings/tests/test_statevector_mapping.py
+++ tangelo/toolboxes/qubit_mappings/tests/test_statevector_mapping.py
@@ -16,15 +16,22 @@
 fermionic occupation of reference state into qubit representation.
 """
 
+
 import unittest
+from itertools import combinations
+
 import numpy as np
 
 from tangelo.toolboxes.qubit_mappings.statevector_mapping import get_vector, vector_to_circuit
-from tangelo.molecule_library import mol_H4_sto3g, mol_H4_cation_sto3g
+from tangelo.toolboxes.molecular_computation.molecule import SecondQuantizedMolecule
+from tangelo.molecule_library import mol_H4_sto3g, mol_H4_cation_sto3g, xyz_H2O
 from tangelo.toolboxes.qubit_mappings.mapping_transform import fermion_to_qubit_mapping
 from tangelo.linq import Simulator
 
 sim = Simulator()
+spin_ind_transforms = ["BK", "JW", "JKMN"]
+spin_dep_transforms = ["SCBK"]
+all_transforms = spin_ind_transforms + spin_dep_transforms
 
 
 class TestVector(unittest.TestCase):
@@ -70,128 +77,85 @@ class TestVector(unittest.TestCase):
         self.assertEqual(circuit.size, sum(vector))
         self.assertEqual(circuit.width, vector.size)
 
-    def test_all_same_energy_mol_H4_sto3g(self):
+    def test_all_same_energy_mol_H4_sto3g_and_mol_H2O_sto3g(self):
         """Check that all mappings return statevectors that have the same energy expectation
-        for an even number of electrons and various spins"""
-        ferm_op = mol_H4_sto3g.fermionic_hamiltonian
-        qu_op_bk = fermion_to_qubit_mapping(ferm_op,
-                                            "BK",
-                                            mol_H4_sto3g.n_active_sos,
-                                            mol_H4_sto3g.n_active_electrons,
-                                            up_then_down=True)
-        qu_op_jw = fermion_to_qubit_mapping(ferm_op,
-                                            "JW",
-                                            mol_H4_sto3g.n_active_sos,
-                                            mol_H4_sto3g.n_active_electrons,
-                                            up_then_down=True)
-        qu_op_jkmn = fermion_to_qubit_mapping(ferm_op,
-                                              "JKMN",
-                                              mol_H4_sto3g.n_active_sos,
-                                              mol_H4_sto3g.n_active_electrons,
-                                              up_then_down=True)
+        for an even number of electrons and various spins. Molecules tested are H4, and H2O
+        with frozen_orbitals=[0, 7] which failed previously for scbk"""
+        mols = [mol_H4_sto3g, SecondQuantizedMolecule(xyz_H2O, 0, 0, "sto-3g", frozen_orbitals=[0, 7])]
+        circuits = dict()
+        qu_ops = dict()
+        for mol in mols:
+            ferm_op = mol.fermionic_hamiltonian
+            qu_ops = dict()
+            for transform in spin_ind_transforms:
+                qu_ops[transform] = fermion_to_qubit_mapping(ferm_op,
+                                                             transform,
+                                                             mol.n_active_sos,
+                                                             mol.n_active_electrons,
+                                                             up_then_down=True)
 
-        # Test for spin 0, 2, and 4
-        for spin in range(3):
-            vector_bk = get_vector(mol_H4_sto3g.n_active_sos,
-                                   mol_H4_sto3g.n_active_electrons,
-                                   mapping="BK",
-                                   up_then_down=True,
-                                   spin=spin*2)
-            vector_scbk = get_vector(mol_H4_sto3g.n_active_sos,
-                                     mol_H4_sto3g.n_active_electrons,
-                                     mapping="SCBK",
-                                     up_then_down=True,
-                                     spin=spin*2)
-            vector_jw = get_vector(mol_H4_sto3g.n_active_sos,
-                                   mol_H4_sto3g.n_active_electrons,
-                                   mapping="JW",
-                                   up_then_down=True,
-                                   spin=spin*2)
-            vector_jkmn = get_vector(mol_H4_sto3g.n_active_sos,
-                                     mol_H4_sto3g.n_active_electrons,
-                                     mapping="JKMN",
-                                     up_then_down=True,
-                                     spin=spin*2)
-            circuit_bk = vector_to_circuit(vector_bk)
-            circuit_scbk = vector_to_circuit(vector_scbk)
-            circuit_jw = vector_to_circuit(vector_jw)
-            circuit_jkmn = vector_to_circuit(vector_jkmn)
+            # Test for spin 0, 2, and 4
+            for spin in range(3):
+                # Get circuits for transforms not dependant on spin
+                for transform in all_transforms:
+                    vector = get_vector(mol.n_active_sos,
+                                        mol.n_active_electrons,
+                                        mapping=transform,
+                                        up_then_down=True,
+                                        spin=spin*2)
+                    circuits[transform] = vector_to_circuit(vector)
 
-            qu_op_scbk = fermion_to_qubit_mapping(ferm_op,
-                                                  'SCBK',
-                                                  mol_H4_sto3g.n_active_sos,
-                                                  mol_H4_sto3g.n_active_electrons,
-                                                  up_then_down=True,
-                                                  spin=spin*2)
+                for transform in spin_dep_transforms:
+                    qu_ops[transform] = fermion_to_qubit_mapping(ferm_op,
+                                                                 'SCBK',
+                                                                 mol.n_active_sos,
+                                                                 mol.n_active_electrons,
+                                                                 up_then_down=True,
+                                                                 spin=spin*2)
 
-            e_bk = sim.get_expectation_value(qu_op_bk, circuit_bk)
-            e_scbk = sim.get_expectation_value(qu_op_scbk, circuit_scbk)
-            e_jw = sim.get_expectation_value(qu_op_jw, circuit_jw)
-            e_jkmn = sim.get_expectation_value(qu_op_jkmn, circuit_jkmn)
-            self.assertAlmostEqual(e_bk, e_jw, places=7, msg=f"Failed for bk vs jw for spin={spin}")
-            self.assertAlmostEqual(e_jw, e_scbk, places=7, msg=f"Failed for jw vs scbk for spin={spin}")
-            self.assertAlmostEqual(e_scbk, e_jkmn, places=7, msg=f"Failed for jkmn vs scbk for spin={spin}")
+                energies = dict()
+                for transform in all_transforms:
+                    energies[transform] = sim.get_expectation_value(qu_ops[transform], circuits[transform])
+
+                for t1, t2 in combinations(all_transforms, 2):
+                    self.assertAlmostEqual(energies[t1], energies[t2], places=7, msg=f"Failed for {t1} vs {t2} for spin={spin*2}")
 
     def test_all_same_energy_mol_H4_cation_sto3g(self):
         """Check that all mappings return statevectors that have the same energy expectation
         for an odd number of electrons and various spins"""
         ferm_op = mol_H4_cation_sto3g.fermionic_hamiltonian
-        qu_op_bk = fermion_to_qubit_mapping(ferm_op,
-                                            "BK",
-                                            mol_H4_cation_sto3g.n_active_sos,
-                                            mol_H4_cation_sto3g.n_active_electrons,
-                                            up_then_down=True)
-        qu_op_jw = fermion_to_qubit_mapping(ferm_op,
-                                            "JW",
-                                            mol_H4_cation_sto3g.n_active_sos,
-                                            mol_H4_cation_sto3g.n_active_electrons,
-                                            up_then_down=True)
-        qu_op_jkmn = fermion_to_qubit_mapping(ferm_op,
-                                              "JKMN",
-                                              mol_H4_cation_sto3g.n_active_sos,
-                                              mol_H4_cation_sto3g.n_active_electrons,
-                                              up_then_down=True)
+        circuits = dict()
+        qu_ops = dict()
+        for transform in spin_ind_transforms:
+            qu_ops[transform] = fermion_to_qubit_mapping(ferm_op,
+                                                         transform,
+                                                         mol_H4_cation_sto3g.n_active_sos,
+                                                         mol_H4_cation_sto3g.n_active_electrons,
+                                                         up_then_down=True)
+
         # Test for spin 1 and 3
         for spin in range(2):
-            vector_bk = get_vector(mol_H4_cation_sto3g.n_active_sos,
-                                   mol_H4_cation_sto3g.n_active_electrons,
-                                   mapping="BK",
-                                   up_then_down=True,
-                                   spin=spin*2+1)
-            vector_scbk = get_vector(mol_H4_cation_sto3g.n_active_sos,
-                                     mol_H4_cation_sto3g.n_active_electrons,
-                                     mapping="SCBK",
-                                     up_then_down=True,
-                                     spin=spin*2+1)
-            vector_jw = get_vector(mol_H4_cation_sto3g.n_active_sos,
-                                   mol_H4_cation_sto3g.n_active_electrons,
-                                   mapping="JW",
-                                   up_then_down=True,
-                                   spin=spin*2+1)
-            vector_jkmn = get_vector(mol_H4_cation_sto3g.n_active_sos,
-                                     mol_H4_cation_sto3g.n_active_electrons,
-                                     mapping="JKMN",
-                                     up_then_down=True,
-                                     spin=spin*2+1)
-            circuit_bk = vector_to_circuit(vector_bk)
-            circuit_scbk = vector_to_circuit(vector_scbk)
-            circuit_jw = vector_to_circuit(vector_jw)
-            circuit_jkmn = vector_to_circuit(vector_jkmn)
+            for transform in all_transforms:
+                vector = get_vector(mol_H4_cation_sto3g.n_active_sos,
+                                    mol_H4_cation_sto3g.n_active_electrons,
+                                    mapping=transform,
+                                    up_then_down=True,
+                                    spin=spin*2+1)
+                circuits[transform] = vector_to_circuit(vector)
+            for transform in spin_dep_transforms:
+                qu_ops[transform] = fermion_to_qubit_mapping(ferm_op,
+                                                             transform,
+                                                             mol_H4_cation_sto3g.n_active_sos,
+                                                             mol_H4_cation_sto3g.n_active_electrons,
+                                                             up_then_down=True,
+                                                             spin=spin*2+1)
 
-            qu_op_scbk = fermion_to_qubit_mapping(ferm_op,
-                                                  'SCBK',
-                                                  mol_H4_cation_sto3g.n_active_sos,
-                                                  mol_H4_cation_sto3g.n_active_electrons,
-                                                  up_then_down=True,
-                                                  spin=spin*2+1)
+            energies = dict()
+            for transform in all_transforms:
+                energies[transform] = sim.get_expectation_value(qu_ops[transform], circuits[transform])
 
-            e_bk = sim.get_expectation_value(qu_op_bk, circuit_bk)
-            e_scbk = sim.get_expectation_value(qu_op_scbk, circuit_scbk)
-            e_jw = sim.get_expectation_value(qu_op_jw, circuit_jw)
-            e_jkmn = sim.get_expectation_value(qu_op_jkmn, circuit_jkmn)
-            self.assertAlmostEqual(e_bk, e_jw, places=7, msg=f"Failed for bk vs jw for spin={spin}")
-            self.assertAlmostEqual(e_jw, e_scbk, places=7, msg=f"Failed for jw vs scbk for spin={spin}")
-            self.assertAlmostEqual(e_scbk, e_jkmn, places=7, msg=f"Failed for scbk vs jkmn for spin={spin}")
+            for t1, t2 in combinations(all_transforms, 2):
+                self.assertAlmostEqual(energies[t1], energies[t2], places=7, msg=f"Failed for {t1} vs {t2} for spin={spin*2}")
 
 
 if __name__ == "__main__":
