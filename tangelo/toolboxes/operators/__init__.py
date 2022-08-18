6fa53882a05e87005cc68d3655d0047772ff4873
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2022-08-18 17:35:50 -0400

    Init update.

--- tangelo/toolboxes/operators/__init__.py
+++ tangelo/toolboxes/operators/__init__.py
@@ -15,4 +15,3 @@
 from .operators import FermionOperator, QubitOperator, QubitHamiltonian
 from .operators import count_qubits, normal_ordered, squared_normal_ordered, list_to_fermionoperator, qubitop_to_qubitham
 from .multiformoperator import MultiformOperator
-from .translation import translate_operator
