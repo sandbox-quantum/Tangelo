f4d46ad0a5bb6c10b456e1d24dc8d8c8546837e7
James Brown <84878946+JamesB-1qbit@users.noreply.github.com>
2022-11-28 16:10:34 -0500

    added multi-product, grid_circuits and discrete_clock (#257)
    * added multi-product, grid_circuits and discrete_clock

---
+++ tangelo/toolboxes/circuits/multiproduct.py
@@ -0,0 +1,133 @@
+# Copyright 2021 Good Chemistry Company.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module to generate the circuits necessary to implement multi-product time-evolution
+Refs:
+    [1] Guang Hao Low, Vadym Kliuchnikov and Nathan Wiebe, "Well-conditioned multi-product
+    Hamiltonian simulation" arXiv: 1907.11679
+"""
+
+import math
+from typing import Union, Tuple, List, Callable
+
+import numpy as np
+
+from tangelo.linq import Circuit, Gate
+from tangelo.linq.helpers.circuits.statevector import StateVector
+from tangelo.toolboxes.ansatz_generator.ansatz_utils import trotterize
+from tangelo.toolboxes.circuits.lcu import sign_flip
+from tangelo.toolboxes.operators import QubitOperator, FermionOperator
+
+
+def get_ajs_kjs(order: int) -> Tuple[List[float], List[int], int]:
+    """Return aj coefficients and number of steps kj for multi-product order
+    The first two indices of aj coefficients are the portion need to make the one-norm sum to two.
+
+    Args:
+        order (int): The desired order of expansion
+
+    Returns:
+        List[float], List[int], int: aj coefficients, kj steps, number of ancilla qubits needed
+    """
+
+    if not isinstance(order, (int, np.integer)):
+        raise TypeError("order must be of integer type")
+    if order < 1 or order > 6:
+        raise ValueError("Tangelo currently only supports orders between 1 and 6")
+
+    mp_qus = math.ceil(np.log2(order+2))
+
+    adict = {1: [1],
+             2: [1/3, 4/3],
+             3: [1/105, 1/6, 81/70],
+             4: [1/2376, 2/45, 729/3640, 31250/27027],
+             5: [1/165888, 256/89775, 6561/179200, 390625/2128896, 6975757441/6067353600],
+             6: [1/5544000, 8/19665, 81/4480, 65536/669375, 216/875, 7626831723/6537520000]}
+    kdict = {1: [1],
+             2: [1, 2],
+             3: [1, 2, 6],
+             4: [1, 2, 3, 10],
+             5: [1, 2, 3, 5, 17],
+             6: [1, 2, 3, 4, 6, 21]}
+    fac = sum(adict[order])
+    vlen = 2**mp_qus
+    ajs = np.sqrt(np.abs([(2 - fac)/2, (2 - fac)/2] + [0]*(vlen-2-order) + adict[order]))
+    ajs /= np.linalg.norm(ajs)
+    kjs = [0]*(vlen-order) + kdict[order]
+    return list(ajs), kjs, mp_qus
+
+
+def get_multi_product_circuit(time: float, order: int, n_state_qus: int,
+                              operator: Union[None, QubitOperator, FermionOperator] = None,
+                              control: Union[int, list] = None,
+                              second_order_trotter: Union[None, Callable[..., Circuit]] = None,
+                              trotter_kwargs: Union[dict, None] = None) -> Circuit:
+    """Return multi-product circuit as defined in arXiv: 1907.11679. Only up to 6th order is currently supported
+
+    Args:
+        time (float): The time to evolve.
+        order (int): The order of the multi-product expansion
+        n_state_qus (int): The number of qubits in the state to evolve.
+        operator (Union[QubitOperator, FermionOperator]): The operator to evolve in time. Default None
+        control (Union[int, List[int]]): The control qubit(s). Default None
+        second_order_trotter (Callable[..., Circuit]): The callable function that defines the controlled 2nd order
+            time-evolution. Must have arguments "control" and "n_trotter_steps".
+        trotter_kwargs (dict): Other keyword arguments necessary to evaluate second_order_trotter.
+
+    Returns:
+        Circuit: The circuit representing the time-evolution using the multi-product construction.
+    """
+
+    if second_order_trotter is None:
+        if operator is None:
+            raise ValueError("Must supply second_order_trotter function or operator.")
+        second_order_trotter = trotterize
+        if trotter_kwargs is None:
+            trotter_kwargs = {"operator": operator, "time": time, "trotter_order": 2}
+
+    if control is not None:
+        cont_list = control if isinstance(control, list) else [control]
+    else:
+        cont_list = []
+
+    ajs, kjs, n_mp_qus = get_ajs_kjs(order)
+    prep_qus = list(range(n_state_qus, n_state_qus+n_mp_qus))
+    prep_state = StateVector(ajs, order="lsq_first")
+    prep_circ = prep_state.initializing_circuit()
+    prep_circ.reindex_qubits(prep_qus)
+
+    ctrott = prep_circ
+    for ii in range(2**n_mp_qus-1, 2**n_mp_qus-order-1, -1):
+        birep = np.binary_repr(ii, width=n_mp_qus)
+        x2_ladder = Circuit([Gate("X", c+n_state_qus) for c, j in enumerate(birep) if j == "0"])
+        ctrott += x2_ladder
+        ctrott += second_order_trotter(control=cont_list+prep_qus, n_trotter_steps=kjs[ii], **trotter_kwargs)
+        # Add -1 phase for every other term in multi-product expansion
+        if ii % 2 == 0:
+            ctrott += Circuit([Gate("CRZ", 0, parameter=2*np.pi, control=cont_list+prep_qus)])
+        ctrott += x2_ladder
+
+    # add -I term for oblivious amplitude amplification
+    birep = np.binary_repr(0, width=n_mp_qus)
+    x2_ladder = Circuit([Gate("X", c+n_state_qus) for c, j in enumerate(birep) if j == "0"])
+    ctrott += x2_ladder + Circuit([Gate("CRZ", 0, parameter=2*np.pi, control=cont_list+prep_qus)]) + x2_ladder
+
+    ctrott += prep_circ.inverse()
+
+    flip = sign_flip(prep_qus, control=cont_list)
+    oaa_mp_circuit = ctrott + flip + ctrott.inverse() + flip + ctrott
+    if control is not None:
+        oaa_mp_circuit += Circuit([Gate("CRZ", 0, control=control, parameter=-2*np.pi)])
+
+    return oaa_mp_circuit
---
+++ tangelo/toolboxes/circuits/tests/test_discrete_clock.py
@@ -0,0 +1,122 @@
+# Copyright 2021 Good Chemistry Company.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import math
+
+from openfermion import get_sparse_operator
+import numpy as np
+from scipy.linalg import expm
+
+from tangelo.linq import get_backend, Circuit
+from tangelo.helpers.utils import installed_backends
+from tangelo.linq.helpers.circuits.statevector import StateVector
+from tangelo.toolboxes.operators.operators import QubitOperator
+from tangelo.toolboxes.qubit_mappings.mapping_transform import fermion_to_qubit_mapping
+from tangelo.toolboxes.ansatz_generator.ansatz_utils import get_qft_circuit, trotterize
+from tangelo.molecule_library import mol_H2_sto3g
+from tangelo.toolboxes.circuits.discrete_clock import get_discrete_clock_circuit
+from tangelo.toolboxes.circuits.grid_circuits import get_psquared_circuit, get_xsquared_circuit
+
+# Test for both "cirq" and if available "qulacs". These have different orderings.
+# qiskit is not currently supported because does not have multi controlled general gates.
+backends = ["cirq", "qulacs"] if "qulacs" in installed_backends else ["cirq"]
+# Initiate Simulator using cirq for phase estimation tests as it has the same ordering as openfermion
+# and we are using an exact eigenvector for testing.
+sim_cirq = get_backend("cirq")
+
+
+class DiscreteClockTest(unittest.TestCase):
+
+    def test_time_independant_hamiltonian(self):
+        """Test time-evolution of discrete clock for a time-independant Hamiltonian"""
+
+        qu_op = fermion_to_qubit_mapping(mol_H2_sto3g.fermionic_hamiltonian, "scbk", mol_H2_sto3g.n_active_sos, mol_H2_sto3g.n_active_electrons,
+                                         True, 0)
+
+        ham = get_sparse_operator(qu_op).toarray()
+        _, vecs = np.linalg.eigh(ham)
+        vec = (vecs[:, 0] + vecs[:, 1])/np.sqrt(2)
+
+        time = 10.
+        exact = expm(-1j*ham*time)@vec
+
+        def trotter_func(t0, time, n_trotter_steps, control):
+            return trotterize(operator=qu_op, time=time, n_trotter_steps=n_trotter_steps, control=control, trotter_order=2)
+
+        for backend in backends:
+            sim = get_backend(backend)
+            statevector_order = sim.backend_info()["statevector_order"]
+            sv = StateVector(vec, order=statevector_order)
+            sv_circuit = sv.initializing_circuit()
+
+            for k in [2, 3]:
+                taylor_circuit = get_discrete_clock_circuit(trotter_func=trotter_func, trotter_kwargs={}, time=time, mp_order=k, n_state_qus=2,
+                                                            n_time_steps=4)
+                _, v = sim.simulate(sv_circuit + taylor_circuit, return_statevector=True)
+                n_ancilla = 2 + math.ceil(np.log2(k+2))
+                len_ancilla = 2**n_ancilla
+                v = v.reshape([4, len_ancilla])[:, 0] if statevector_order == "lsq_first" else v.reshape([len_ancilla, 4])[0, :]
+                self.assertAlmostEqual(1, np.abs(v.conj().dot(exact)), delta=1.e-1**k)
+
+    def test_time_dependant_hamiltonian(self):
+        """Test time-evolution of discrete clock for a time-dependant Hamiltonian taken from
+        arXiv: 1412.1802 H = 1/2/m * p^2 + (4*exp(-2*t) - 1/16) * x^2 - 2*exp(-t) with mass=1/2
+        and exact answer (2/pi)^(1/4)*exp(-x^2*exp(-t) - 1/4*t + 1j/8*x^2)"""
+
+        n_qubits = 6
+        n_pts = 2**n_qubits
+        dx = 0.2
+        x0 = dx*(n_pts//2 - 1/2)
+        gridpts = np.linspace(-x0, x0, n_pts)
+        mass = 1/2
+        time = 1.
+
+        def psiexact(xpts, t):
+            return (2/np.pi)**(1/4)*np.exp(-xpts**2*np.exp(-t)-1/4*t+1j/8*xpts**2)*np.sqrt(dx)
+
+        exact = psiexact(gridpts, time)
+
+        def trotter_func(t0, time, n_trotter_steps, control, dx, qubit_list):
+            circ = Circuit()
+            dt = time/n_trotter_steps
+            p2 = get_psquared_circuit(dt/2, dx, mass, qubit_list, control)
+            for i in range(n_trotter_steps):
+                th = t0 + (i + 1/2) * dt
+                circ += p2
+                circ += get_xsquared_circuit(dt, dx, (4*np.exp(-2*th) - 1/16), x0, -2*np.exp(-th), qubit_list, control)
+                circ += p2
+            return circ
+
+        for backend in backends:
+            sim = get_backend(backend)
+            statevector_order = sim.backend_info()["statevector_order"]
+            vec = psiexact(gridpts, 0)
+            sv = StateVector(vec, order=statevector_order)
+            sv_circuit, phase = sv.initializing_circuit(return_phase=True)
+
+            for k in [2, 3]:
+                qubit_list = list(reversed(range(n_qubits))) if statevector_order == "lsq_first" else list((range(n_qubits)))
+                taylor_circuit = get_discrete_clock_circuit(trotter_func=trotter_func, trotter_kwargs={"dx": dx, "qubit_list": qubit_list}, time=time,
+                                                            mp_order=k, n_state_qus=6,
+                                                            n_time_steps=2)
+                _, v = sim.simulate(sv_circuit + taylor_circuit, return_statevector=True)
+                n_ancilla = 1 + math.ceil(np.log2(k+2))
+                len_ancilla = 2**n_ancilla
+                v = v.reshape([n_pts, len_ancilla])[:, 0] if statevector_order == "lsq_first" else v.reshape([len_ancilla, n_pts])[0, :]
+                self.assertAlmostEqual(1, (v.conj().dot(exact)*np.exp(-1j*phase)).real, delta=1.e-1**k)
+
+
+if __name__ == "__main__":
+    unittest.main()
