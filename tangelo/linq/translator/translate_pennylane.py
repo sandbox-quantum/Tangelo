21ccbbed0e8e355f47d5e7998a82a7ef362eb361
James Brown <james.brown@1qbit.com>
2022-12-15 11:58:28 -0500

    PR fixes

--- tangelo/linq/translator/translate_pennylane.py
+++ tangelo/linq/translator/translate_pennylane.py
@@ -24,12 +24,10 @@ necessary to account for:
 
 from math import pi
 
-from tangelo.helpers import deprecated
-
 
 def get_pennylane_gates():
     """Map gate name of the abstract format to the equivalent methods of the
-    cirq class API and supported gates: https://quantumai.google/pennylane/gates.
+    pennylane class API and supported gates:https://docs.pennylane.ai/en/stable/code/qml.html#classes.
     """
     import pennylane as qml
 
@@ -55,73 +53,48 @@ def get_pennylane_gates():
     GATE_PENNYLANE["XX"] = qml.IsingXX
     GATE_PENNYLANE["SWAP"] = qml.SWAP
     GATE_PENNYLANE["CSWAP"] = qml.CSWAP
+    # TODO: Check periodically if better support for measure gates is implemented
     # GATE_PENNYLANE["MEASURE"] = qml.measure  # Pennylane currently only supports measuring a qubit once
     return GATE_PENNYLANE
 
 
-@deprecated("Please use the translate_circuit function.")
-def translate_pennylane(source_circuit):
-    """Take in an abstract circuit, return an equivalent pennylane QuantumCircuit
-    instance.
+def translate_c_to_pennylane(source_circuit):
+    """Take in an tangelo Circuit, return an equivalent pennylane List[Type[pennylane.operation.Operation]].
 
     Args:
-        source_circuit: quantum circuit in the abstract format.
+        source_circuit (Circuit): quantum circuit in the abstract format.
 
     Returns:
-        pennylane.Circuit: a corresponding pennylane Circuit. Right now, the structure is
-            of LineQubit. It is possible in the future that we may support
-            NamedQubit or GridQubit.
+        List[Type[pennylane.operator.Operation]]: a corresponding List of child classes of pennylane.operation.Operation.
     """
-    return translate_c_to_pennylane(source_circuit)
-
-
-def translate_c_to_pennylane(source_circuit, noise_model=None):
-    """Take in an abstract circuit, return an equivalent pennylane QuantumCircuit
-    object.
-
-    Args:
-        source_circuit: quantum circuit in the abstract format.
-
-    Returns:
-        List[qml.ops]: a corresponding List of ops.
-    """
-    import pennylane as qml
 
     GATE_PENNYLANE = get_pennylane_gates()
     target_circuit = []
 
     # Maps the gate information properly. Different for each backend (order, values)
-    for gate in source_circuit._gates:
+    for gate in source_circuit:
         if gate.control is not None and len(gate.control) > 1 and gate.name != "CX":
             raise ValueError(f"Can not use {gate.name} with multiple controls. Only CX translates properly to pennylane")
         if gate.name in {"H", "X", "Y", "Z", "S", "T"}:
             target_circuit.append(GATE_PENNYLANE[gate.name](gate.target[0]))
-        elif gate.name in {"CY", "CZ"}:
+        elif gate.name in {"CY", "CZ", "CNOT"}:
             target_circuit.append(GATE_PENNYLANE[gate.name]([gate.control[0], gate.target[0]]))
         elif gate.name in {"CX"}:
             target_circuit.append(GATE_PENNYLANE[gate.name](wires=[*gate.control, gate.target[0]]))
         elif gate.name in {"CH"}:
             target_circuit.append(GATE_PENNYLANE["CNOT"]([gate.control[0], gate.target[0]]))
             target_circuit.append(GATE_PENNYLANE["CRY"](-pi/2, [gate.control[0], gate.target[0]]))
-        elif gate.name in {"RX", "RY", "RZ"}:
+        elif gate.name in {"RX", "RY", "RZ", "PHASE"}:
             target_circuit.append(GATE_PENNYLANE[gate.name](gate.parameter, gate.target[0]))
-        elif gate.name in {"CNOT"}:
-            target_circuit.append(GATE_PENNYLANE[gate.name]([gate.control[0], gate.target[0]]))
-        # elif gate.name in {"MEASURE"}:
-        #    target_circuit.append(GATE_PENNYLANE[gate.name](gate.target[0]))
-        elif gate.name in {"CRZ", "CRX", "CRY"}:
+        elif gate.name in {"CRZ", "CRX", "CRY", "CPHASE"}:
             target_circuit.append(GATE_PENNYLANE[gate.name](gate.parameter, [gate.control[0], gate.target[0]]))
         elif gate.name in {"XX"}:
             target_circuit.append(GATE_PENNYLANE[gate.name](gate.parameter, [gate.target[0], gate.target[1]]))
-        elif gate.name in {"PHASE"}:
-            target_circuit.append(GATE_PENNYLANE[gate.name](gate.parameter, gate.target[0]))
-        elif gate.name in {"CPHASE"}:
-            target_circuit.append(GATE_PENNYLANE[gate.name](gate.parameter, [gate.control[0], gate.target[0]]))
         elif gate.name in {"SWAP"}:
             target_circuit.append(GATE_PENNYLANE[gate.name]([gate.target[0], gate.target[1]]))
         elif gate.name in {"CSWAP"}:
             target_circuit.append(GATE_PENNYLANE[gate.name]([gate.control[0], gate.target[0], gate.target[1]]))
         else:
-            raise ValueError(f"Gate '{gate.name}' not supported on backend pennylane")
+            raise ValueError(f"Gate '{gate.name}' not supported for translation to pennylane")
 
     return target_circuit
