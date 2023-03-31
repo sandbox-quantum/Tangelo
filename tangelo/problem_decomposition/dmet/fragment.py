748d8619e5dda0c6b203ce4b7073f8acb3927443
Alexandre Fleury <76115575+AlexandreF-1qbit@users.noreply.github.com>
2023-03-31 13:33:19 -0400

    Re-enabling open-shell DMET (#291)
    * Re-enabling OS-DMET.

--- tangelo/problem_decomposition/dmet/fragment.py
+++ tangelo/problem_decomposition/dmet/fragment.py
@@ -67,7 +67,7 @@ class SecondQuantizedDMETFragment:
 
         self.basis = self.molecule.basis
 
-        self.n_mos = len(self.mean_field.mo_energy)
+        self.n_mos = len(self.mean_field.mo_energy[0]) if self.uhf else len(self.mean_field.mo_energy)
         self.mo_occ = self.mean_field.mo_occ
 
         list_of_active_frozen = convert_frozen_orbitals(self, self.frozen_orbitals)
