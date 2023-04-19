60c31bfad40e800be8a20851f683811efda21218
James Brown <james.brown@1qbit.com>
2023-04-19 11:08:13 -0400

    small test for dummy solver

--- tangelo/toolboxes/molecular_computation/tests/test_molecule.py
+++ tangelo/toolboxes/molecular_computation/tests/test_molecule.py
@@ -21,6 +21,7 @@ from openfermion.utils import load_operator
 from tangelo import SecondQuantizedMolecule
 from tangelo.molecule_library import mol_H2_sto3g, xyz_H2O
 from tangelo.toolboxes.molecular_computation.molecule import atom_string_to_list
+from tangelo.toolboxes.molecular_computation.essolver import ESSolver
 
 # For openfermion.load_operator function.
 pwd_this_test = os.path.dirname(os.path.abspath(__file__))
@@ -162,6 +163,55 @@ class SecondQuantizedMoleculeTest(unittest.TestCase):
         bad_dummy_mo_coeff = [np.ones((3, 3))]*2
         with self.assertRaises(AssertionError):
             molecule.mo_coeff = bad_dummy_mo_coeff
+
+    def test_custom_solver_H2(self):
+        """Verify that using a custom ESSolver that only stores molecular data functions correctly returns
+        energy_from_rdms and fermion_hamiltonian
+        """
+
+        molecule = SecondQuantizedMolecule(H2_list, 0, 0, basis="sto-3g")
+
+        core_constant, one_body_integrals, two_body_integrals = molecule.get_full_space_integrals()
+
+        class ESSolver_dummy(ESSolver):
+            def set_basic_data(self, mol):
+                mol.xyz = H2_list
+                mol.n_electrons = 2
+                mol.n_atoms = 2
+
+            def compute_mean_field(self, sqmol):
+                sqmol.mf_energy = molecule.mf_energy
+                sqmol.mo_energies = molecule.mo_energies
+                sqmol.mo_occ = molecule.mo_occ
+                sqmol.n_mos = molecule.n_mos
+                sqmol.n_sos = molecule.n_sos
+                self.mo_coeff = molecule.mo_coeff.copy()
+
+            def get_integrals(self, sqmol, mo_coeff=None):
+                return core_constant, one_body_integrals, two_body_integrals
+
+        molecule_dummy = SecondQuantizedMolecule(H2_list, 0, 0, ESSolver_dummy(), basis="sto-3g", frozen_orbitals=[])
+
+        rdm1 = [[ 1.97453997e+00, -7.05987336e-17],
+                [-7.05987336e-17,  2.54600303e-02]]
+
+        rdm2 = [
+            [[[ 1.97453997e+00, -7.96423130e-17],
+              [-7.96423130e-17,  3.21234218e-33]],
+             [[-7.96423130e-17, -2.24213843e-01],
+              [ 0.00000000e+00,  9.04357944e-18]]],
+            [[[-7.96423130e-17,  0.00000000e+00],
+              [-2.24213843e-01,  9.04357944e-18]],
+             [[ 3.21234218e-33,  9.04357944e-18],
+              [ 9.04357944e-18,  2.54600303e-02]]]
+            ]
+
+        e_rdms = molecule_dummy.energy_from_rdms(rdm1, rdm2)
+        self.assertAlmostEqual(e_rdms, -1.1372701, delta=1e-5)
+
+        ferm_op = molecule_dummy.fermionic_hamiltonian
+        ref_ferm_op = load_operator("H2_ferm_op.data", data_directory=pwd_this_test+"/data", plain_text=True)
+        self.assertEqual(ferm_op, ref_ferm_op)
 
 
 if __name__ == "__main__":
