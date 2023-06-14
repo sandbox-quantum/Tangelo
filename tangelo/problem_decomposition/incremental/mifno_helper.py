4ba97c4329c98bed52d5942f7dc78e5ea67f2fd3
Valentin Senicourt <41597680+ValentinS4t1qbit@users.noreply.github.com>
2023-06-14 01:16:17 -0700

    Update mifno_helper.py

--- tangelo/problem_decomposition/incremental/mifno_helper.py
+++ tangelo/problem_decomposition/incremental/mifno_helper.py
@@ -171,7 +171,6 @@ class MIFNOHelper():
         """
 
         data_fragments = self.to_dataframe
-        data_fragments.drop(["problem_handle"], axis=1, inplace=True)
         data_fragments.drop(["frozen_orbitals_truncated"], axis=1, inplace=True)
         data_fragments.drop(["complete_orbital_space"], axis=1, inplace=True)
         str_rep = f"(All the energy values are in hartree)\n" \
