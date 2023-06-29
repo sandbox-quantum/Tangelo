5f93ba45721f77595633cc2537c3574fc9a391e7
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-06-29 10:27:08 -0400

    Fixed requirements
    Added missing requirements pyscf and pyscf-semiempirical (depends on
    https://github.com/pyscf/semiempirical/issues/8)
    Removed unused setup dep
    Removed pre-install dep already handled by setuptools

--- setup.py
+++ setup.py
@@ -3,18 +3,11 @@ import sys
 import subprocess
 import os
 
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-
 with open("tangelo/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
 with open('README.rst', 'r') as f:
     long_description = f.read()
-
-install('wheel')
 
 description = "Maintained by Good Chemistry Company, focusing on the development of end-to-end materials simulation workflows on quantum computers."
 
@@ -24,10 +17,8 @@ setuptools.setup(
     version=version,
     description=description,
     long_description=description,
-    # long_description_content_type=description,
     url="https://github.com/goodchemistryco/Tangelo",
     packages=setuptools.find_packages(),
     test_suite="tangelo",
-    setup_requires=['h5py'],
-    install_requires=['h5py', 'bitarray', 'openfermion']
+    install_requires=['h5py', 'bitarray', 'openfermion', 'openfermionpyscf', 'pyscf', 'pyscf-semiempirical']
 )
