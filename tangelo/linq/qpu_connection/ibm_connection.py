ea93ed4e138d7f9c2f457ef2590e70da82130d89
AlexandreF-1qbit <76115575+AlexandreF-1qbit@users.noreply.github.com>
2023-11-08 19:05:45 +0000

    Typos.

--- tangelo/linq/qpu_connection/ibm_connection.py
+++ tangelo/linq/qpu_connection/ibm_connection.py
@@ -55,7 +55,7 @@ class IBMConnection(QpuConnection):
             circuits (Circuit | List[Circuit]): Tangelo circuit(s)
             operators (QubitOperator | List[QubitOperator]) : Optional, qubit operators for computing expectation values
             runtime_options (dict): Optional, extra keyword arguments for options supported in qiskit-runtime.
-            isntance (str): The service instance to use, as  in hub/group/project in that format.
+            instance (str): The service instance to use, as in hub/group/project in that format.
 
         Returns:
             str: string representing the job id
